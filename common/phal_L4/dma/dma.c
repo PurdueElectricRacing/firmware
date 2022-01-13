0f39627ef3496ca9a4ac13b88419182eb8d5ee5e
Dawson Moore <moore800@purdue.edu>
2022-01-13 13:56:44 -0500

    Add start of DMA and fix gitmodules

---
+++ common/phal_L4/dma/dma.c
@@ -0,0 +1,69 @@
+/**
+ * @file dma.c
+ * @author Dawson Moore (moore800@purdue.edu)
+ * @brief Basic DMA Peripheral HAL library for setting up DMA transfers
+ * @version 0.1
+ * @date 2022-01-10
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "common/phal_L4/dma/dma.h"
+
+bool PHAL_initDMA(dma_init_t* init) {
+    // Check we aren't going to break the peripheral
+    if (init->mem_to_mem && init->circular) {
+        return false;
+    } else if (init->dir > 1) {
+        return false;
+    } else if (init->priority > 3) {
+        return false;
+    } else if (init->mem_size > 2 || init->periph_size > 2) {
+        return false;
+    }
+    
+    // Enable clock in RCC
+    if (init->periph == DMA1) {
+        RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
+    } else if (init->periph == DMA2) {
+        RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
+    } else {
+        return false;
+    }
+
+    // Channel configuration set
+    init->channel->CCR &= ~(1U);
+    init->channel->CCR &= ~(0x7f);
+
+    init->channel->CCR |= (init->mem_to_mem << 14) | (init->priority << 12) | (init->mem_size << 10);
+    init->channel->CCR |= (init->periph_size << 8) | (init->mem_inc << 7) | (init->periph_inc << 6);
+    init->channel->CCR |= (init->circular << 5) | (init->dir << 4) | (1U << 3) | (init->tx_isr_en << 1);
+
+    // Channel memory configuration set
+    init->channel->CNDTR &= ~(0xff);
+    init->channel->CNDTR |= init->tx_size;
+
+    init->channel->CPAR = init->periph_addr;
+    init->channel->CMAR = init->mem_addr;
+
+    init->request->CSELR &= ~(0xf) << ((init->channel_idx - 1) * 4);
+    init->request->CSELR |= (init->dma_chan_request<< ((init->channel_idx - 1) * 4));
+
+    return true;
+}
+
+void PHAL_startTxfer(dma_init_t* init) {
+    // Channel enable starts txfer
+    init->channel->CCR |= 1U;
+}
+
+void PHAL_stopTxfer(dma_init_t* init) {
+    // Channel disable stops txfer
+    init->channel->CCR &= ~1U;
+}
+
+void PHAL_reEnable(dma_init_t* init) {
+    init->periph->IFCR |= (0xf << (init->channel_idx - 1) * 4);
+
+    init->channel->CCR |= 1U;
+}
---
+++ common/phal_L4/dma/dma.h
@@ -0,0 +1,68 @@
+#ifndef _DMA_H_
+#define _DMA_H_
+
+#ifdef STM32L496xx
+#include "stm32l496xx.h"
+#elif STM32L432xx
+#include "stm32l432xx.h"
+#else
+#error "Please define a STM32 arch"
+#endif
+
+#include <stdbool.h>
+
+typedef struct {
+    uint32_t    periph_addr;
+    uint32_t    mem_addr;
+    uint16_t    tx_size;
+
+    bool        increment;
+    bool        circular;
+    uint8_t     dir;
+    bool        mem_inc;
+    bool        periph_inc;
+    bool        mem_to_mem;
+    uint8_t     priority;
+    uint8_t     mem_size;
+    uint8_t     periph_size;
+    bool        tx_isr_en;
+    uint8_t     dma_chan_request; /* Table 44 of Family Reference  */
+    uint8_t     channel_idx;
+
+    DMA_TypeDef* periph;
+    DMA_Channel_TypeDef* channel;
+    DMA_Request_TypeDef* request;
+} dma_init_t;
+
+/*
+ * @brief Initialize DMA peripheral to set m2m, p2p, or p2m with set size
+ *        and length of txfer
+ * 
+ * @param init -> Address of initialization structure
+ * @return true -> Successful init (no clashing params)
+ * @return false -> Init not complete (parameters clash)
+ */
+bool PHAL_initDMA(dma_init_t* init);
+
+/*
+ * @brief Start txfer after sucessful DMA peripheral initialization
+ * 
+ * @param init -> Address of initialization structure
+ */
+void PHAL_startTxfer(dma_init_t* init);
+
+/*
+ * @brief Stop txfer
+ * 
+ * @param init -> Address of initialization structure
+ */
+void PHAL_stopTxfer(dma_init_t* init);
+
+/*
+ * @brief Re-enable DMA txfer after error ISR fires
+ * 
+ * @param init -> Address of initialization structure
+ */
+void PHAL_reEnable(dma_init_t* init);
+
+#endif

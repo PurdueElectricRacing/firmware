c58f1e2daf28ad4f2a2ccda4abc30bb0fe388370
Dawson Moore <moore800@purdue.edu>
2022-01-24 18:55:49 -0500

    Basic blocking stuff

---
+++ common/phal_L4/usart/usart.c
@@ -0,0 +1,118 @@
+/**
+ * @file usart.c
+ * @author Dawson Moore (moore800@purdue.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "common/phal_L4/usart/usart.h"
+
+tx_mode_t mode;
+bool      comp;
+
+bool PHAL_initUSART(const usart_handle_t* handle, const uint32_t fck)
+{
+    // Locals
+    uint16_t div;
+
+    // Disable peripheral until properly configured
+    handle->instance->CR1 &= ~USART_CR1_UE;
+
+    // Enable peripheral clock in RCC
+    switch ((ptr_int) handle->instance) {
+        case USART1_BASE:
+            RCC->AHB2ENR |= RCC_APB2ENR_USART1EN;
+            break;
+
+        case USART2_BASE:
+            RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
+            break;
+
+        case LPUART1_BASE:
+            RCC->APB1ENR2 |= RCC_APB1ENR2_LPUART1EN;
+            break;
+
+        default:
+            return false;
+    }
+
+    // Calculate prescalar for given baud rate
+    div = (fck / handle->baud_rate) * ((handle->ovsample == OV_16) ? 1 : 2);
+
+    // Apply prescalar
+    switch (handle->ovsample) {
+        case OV_16:
+            handle->instance->BRR = div;
+            break;
+
+        case OV_8:
+            handle->instance->BRR = ((div & 0b111) >> 1) | (div & 0xfff0);
+            break;
+    }
+
+    // Set CR1 parameters
+    handle->instance->CR1 =  0U;
+    handle->instance->CR1 |= (handle->word_length & 2) << USART_CR1_M1_Pos;
+    handle->instance->CR1 |= (handle->word_length & 1) << USART_CR1_M0_Pos;
+    handle->instance->CR1 |= handle->ovsample << USART_CR1_OVER8_Pos;
+    handle->instance->CR1 |= handle->parity << USART_CR1_PS_Pos;
+    handle->instance->CR1 |= handle->mode << USART_CR1_RE_Pos;
+
+    // Set CR2 parameters
+    handle->instance->CR2 =  0U;
+    handle->instance->CR2 |= handle->adv_feature.ab_mode << USART_CR2_ABRMODE_Pos;
+    handle->instance->CR2 |= handle->adv_feature.auto_baud << USART_CR2_ABREN_Pos;
+    handle->instance->CR2 |= handle->adv_feature.msb_first << USART_CR2_MSBFIRST_Pos;
+    handle->instance->CR2 |= handle->adv_feature.data_inv << USART_CR2_DATAINV_Pos;
+    handle->instance->CR2 |= handle->adv_feature.tx_inv << USART_CR2_TXINV_Pos;
+    handle->instance->CR2 |= handle->adv_feature.rx_inv << USART_CR2_RXINV_Pos;
+    handle->instance->CR2 |= handle->adv_feature.tx_rx_swp << USART_CR2_SWAP_Pos;
+    handle->instance->CR2 |= handle->stop_bits << USART_CR2_STOP_Pos;
+
+    // Set CR3 parameters
+    handle->instance->CR3 =  0U;
+    handle->instance->CR3 |= handle->adv_feature.dma_on_rx_err << USART_CR3_DDRE_Pos;
+    handle->instance->CR3 |= handle->adv_feature.overrun << USART_CR3_OVRDIS_Pos;
+    handle->instance->CR3 |= handle->obsample << USART_CR3_ONEBIT_Pos;
+
+    // Enable peripheral for use
+    handle->instance->CR1 |= USART_CR1_UE;
+}
+
+void PHAL_usartTxBl(const usart_handle_t* handle, const uint16_t* data, uint32_t len) {
+    uint8_t i;
+    mode = BLOCKING;
+    comp = false;
+    
+    for (i = 0; i < len; i++) {
+        while (!(handle->instance->ISR & USART_ISR_TXE));
+
+        handle->instance->TDR = data[i] & 0x01ff;
+    }
+
+    while (!(handle->instance->ISR & USART_ISR_TC));
+}
+
+void PHAL_usartRxBl(const usart_handle_t* handle, uint16_t* data, uint32_t len) {
+    uint8_t i;
+    mode = BLOCKING;
+    comp = false;
+
+    for (i = 0; i < len; i++) {
+        while (!(handle->instance->ISR & USART_ISR_RXNE));
+
+        data[i] = handle->instance->TDR & 0x01ff;
+    }
+}
+
+bool PHAL_usartTxInt(const usart_handle_t* handle, const uint16_t* data, uint32_t len) {
+
+}
+
+bool PHAL_usartRxint(const usart_handle_t* handle, uint16_t* data, uint32_t len) {
+    
+}
---
+++ common/phal_L4/usart/usart.h
@@ -0,0 +1,113 @@
+/**
+ * @file usart.h
+ * @author Dawson Moore (moore800@purdue.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef _PHAL_USART_H_
+#define _PHAL_USART_H_
+
+// Includes
+#include "stm32l4xx.h"
+#include <stdbool.h>
+
+// Generic Defines
+#define PHAL_USART_TX_TIMEOUT (0xFFFFFFFF)
+#define PHAL_USART_RX_TIMEOUT (0xFFFFFFFF)
+
+typedef uint32_t ptr_int;
+
+// Enumerations
+typedef enum {
+    PT_NONE,
+    PT_EVEN,
+    PT_ODD
+} parity_t;
+
+typedef enum {
+    WORD_8 = 0,
+    WORD_9 = 0x1000,
+    WORD_7 = 0x10000000
+} word_length_t;
+
+typedef enum {
+    SB_ONE,
+    SB_TWO,
+    SB_HALF,
+    SB_ONE_HALF
+} stop_bits_t;
+
+typedef enum {
+    MODE_TX_RX = 0b11,
+    MODE_TX    = 0b10,
+    MODE_RX    = 0b01
+} mode_t;
+
+typedef enum {
+    HW_DISABLE,
+    CTS,
+    RTS,
+    CTS_RTS
+} hw_flow_ctl_t;
+
+typedef enum {
+    OV_16 = 0,
+    OV_8  = 1
+} ovsample_t;
+
+typedef enum {
+    OB_DISABLE,
+    OB_ENABLE
+} obsample_t;
+
+typedef enum {
+    AB_START,
+    AB_FALLING,
+    AB_0X7F,
+    AB_0X55
+} ab_mode_t;
+
+typedef enum {
+    BLOCKING,
+    INTERRUPT,
+    DMA
+} tx_mode_t;
+
+typedef struct {
+    bool      auto_baud;
+    ab_mode_t ab_mode;
+    bool      tx_inv;
+    bool      rx_inv;
+    bool      data_inv;
+    bool      tx_rx_swp;
+    bool      overrun;
+    bool      dma_on_rx_err;
+    bool      msb_first;
+} adv_feature_t;
+
+// Structures
+typedef struct {
+    // Required parameters
+    USART_TypeDef* instance;        // Address of instance
+    uint32_t       baud_rate;       // Baud rate for communication
+    word_length_t  word_length;     // Word length for tx/rx
+    stop_bits_t    stop_bits;       // Number of stop bits to use
+    parity_t       parity;          // Parity of communication
+    mode_t         mode;            // TODO: Find this in the reference
+    hw_flow_ctl_t  hw_flow_ctl;     // TODO: Find this in the reference
+    ovsample_t     ovsample;        // TODO: Find this in the reference
+    obsample_t     obsample;        // TODO: Find this in the reference
+
+    // Advanced features
+    adv_feature_t  adv_feature;     // TODO: Find this in the reference
+} usart_handle_t;
+
+// Function Prototypes
+
+
+#endif

2d5328a19a87098cac7597f739a62790282086ad
Adam Busch <adamjbusch561@gmail.com>
2022-02-21 20:58:35 -0500

    Bootloader project

--- cmake/common_component.cmake
+++ cmake/common_component.cmake
@@ -34,3 +34,33 @@ MACRO(MAKE_COMMON_COMPONENT component_name component_dir common_libs)
     # Run postbuild actions like including a bootloader in the final image
     postbuild_target(${component_name} ${_TARGET_NAME})
 ENDMACRO()
+
+MACRO(MAKE_BOOTLOADER component_name component_dir common_libs linker_script)
+
+    # Setup Component name based on directory
+    set(_TARGET_NAME ${component_name}.elf)
+    add_executable(${_TARGET_NAME})
+
+    # Add Common libraries
+    foreach (_LIB_NAME IN ITEMS ${common_libs})
+        target_link_libraries(${_TARGET_NAME} ${_LIB_NAME})
+    endforeach(_LIB_NAME)
+
+    target_link_libraries(${_TARGET_NAME} common_defs) # everyone gets common defs
+
+    # Find all .c sources in project, recursive search starting at component root
+    file(GLOB_RECURSE glob_sources ${component_dir}/*.c)
+    target_sources(${_TARGET_NAME} PUBLIC ${glob_sources})
+
+    # Find directories for '#include'
+    SUBDIRLIST(${component_dir} include_dirs)
+    target_include_directories(${_TARGET_NAME} PUBLIC ${include_dirs})
+
+    # Linker options
+    target_link_options(${_TARGET_NAME} PUBLIC 
+        -T${COMMON_SOURCE_DIR}/linker/${linker_script}
+    )
+
+    # Run postbuild actions like including a bootloader in the final image
+    postbuild_target(${component_name} ${_TARGET_NAME})
+ENDMACRO()
--- source/bootloader_l4/main.c
+++ source/bootloader_l4/main.c
@@ -1,11 +1,54 @@
 #include "stm32l432xx.h"
 #include "common/phal_L4/can/can.h"
+#include "node_defs.h"
 
 
+bool initCANFilter()
+{
+    CAN1->MCR |= CAN_MCR_INRQ;                // Enter back into INIT state (required for changing scale)
+    uint32_t timeout = 0;
+    while(!(CAN1->MSR & CAN_MSR_INAK) && ++timeout < PHAL_CAN_INIT_TIMEOUT)
+         ;
+    if (timeout == PHAL_CAN_INIT_TIMEOUT)
+         return false;
+
+    CAN1->FMR  |= CAN_FMR_FINIT;              // Enter init mode for filter banks
+    CAN1->FM1R |= 0x07FFFFFF;                 // Set banks 0-27 to id mode
+    CAN1->FS1R |= 0x07FFFFFF;                 // Set banks 0-27 to 32-bit scale
+
+    /* BEGIN AUTO FILTER */
+    CAN1->FA1R |= (1 << 0);    // configure bank 0
+    CAN1->sFilterRegister[0].FR1 = (BOOTLOADER_CAN_ADDR << 3) | 4;
+    /* END AUTO FILTER */
+
+    CAN1->FMR  &= ~CAN_FMR_FINIT;             // Enable Filters (exit filter init mode)
+
+    // Enter back into NORMAL mode
+    CAN1->MCR &= ~CAN_MCR_INRQ;
+    while((CAN1->MSR & CAN_MSR_INAK) && ++timeout < PHAL_CAN_INIT_TIMEOUT)
+         ;
+
+    return timeout != PHAL_CAN_INIT_TIMEOUT;
+}
 
 int main (void)
 {
+    
+    
     PHAL_initCAN(CAN1, false);
+    initCANFilter();
+
+    ApplicationID_t app = COMPILED_APP_ID;
+
+    if (app > APP_INVALID)
+    {
+        asm("bkpt");
+    }
+
+    while (1)
+    {
+        asm("nop");
+    }
 
     return 0;
 }

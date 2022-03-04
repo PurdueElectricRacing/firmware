2d11715af9cc3c537749fe9926291ab5c79274a5
Dawson Moore <moore800@purdue.edu>
2022-03-04 16:36:35 -0500

    Fault structure

---
+++ common/faults/generation/generator.py
@@ -0,0 +1,25 @@
+# Logging helper functions
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def log_error(phrase):
+    print(f"{bcolors.FAIL}ERROR: {phrase}{bcolors.ENDC}")
+
+def log_warning(phrase):
+    print(f"{bcolors.WARNING}WARNING: {phrase}{bcolors.ENDC}")
+
+def log_success(phrase):
+    print(f"{bcolors.OKGREEN}{phrase}{bcolors.ENDC}")
+
+if __name__ == '__main__':
+    log_error('Fault generation not yet implemented')
+
+    quit(0)
---
+++ common/faults/templates/faults.h
@@ -0,0 +1,7 @@
+#ifndef _FAULTS_H_
+#define _FAULTS_H_
+
+// Includes
+#include "stm32l432xx.h"
+
+#endif

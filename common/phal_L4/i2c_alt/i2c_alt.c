d271b731e027b85d49a83f28949956507ed9aaac
Dawson Moore <moore800@purdue.edu>
2022-04-22 17:34:24 -0400

    One step closer

--- common/phal_L4/i2c_alt/i2c_alt.c
+++ common/phal_L4/i2c_alt/i2c_alt.c
@@ -6,10 +6,9 @@ static int  gen_start(I2C_TypeDef* instance, uint8_t addr, uint8_t len, bool rea
 static void i2c_unmask_irq(USART_TypeDef* instance);
 static void i2c_mask_irq(USART_TypeDef* instance);
 
-// Global vars
-i2c_core_t core;
+i2c_handle_t* core;
 
-int PHAL_I2Cinit(I2C_TypeDef* instance, uint32_t freq, const uint32_t fck) {
+int PHAL_I2C_init(I2C_TypeDef* instance, i2c_handle_t* handle, uint32_t freq, const uint32_t fck) {
     uint8_t clk_tim;
     uint8_t presc = 0;
     
@@ -26,16 +25,16 @@ int PHAL_I2Cinit(I2C_TypeDef* instance, uint32_t freq, const uint32_t fck) {
     instance->CR1 &= ~I2C_CR1_PE;
 
     // Enable DMA
-    instance->CR1 |= I2C_CR1_TXDMAEN | I2C_CR1_RXDMAEN | I2C_CR1_TCIE | I2C_CR1_TXIE | I2C_CR1_RXIE;
+    instance->CR1 |= I2C_CR1_TXDMAEN | I2C_CR1_RXDMAEN | I2C_CR1_TCIE | I2C_CR1_TXIE | I2C_CR1_RXIE | I2C_CR1_ERRIE;
 
     // Set clock timing
     instance->TIMINGR &= 0x0f000000;
     clk_tim = fck / (freq * 2) - 1;
 
-    if (clk_tim > 0xf) {
-        presc = 0xf - clk_tim;
+    if (clk_tim > 0xff) {
+        presc = 0xff - clk_tim;
 
-        if (presc > 0xf) {
+        if (presc > 0xff) {
             return -E_CONFIG;
         }
     }
@@ -44,6 +43,19 @@ int PHAL_I2Cinit(I2C_TypeDef* instance, uint32_t freq, const uint32_t fck) {
 
     // Enable peripheral
     instance->CR1 |= I2C_CR1_PE;
+
+    // Configure DMA
+    if (handle->tx_dma_cfg && !PHAL_initDMA(handle->tx_dma_cfg)) {
+        return false;
+    }
+
+    if (handle->rx_dma_cfg && !PHAL_initDMA(handle->rx_dma_cfg)) {
+        return false;
+    }
+
+    // Set core and unmask interrupts
+    core = handle;
+    i2c_unmask_irq(instance);
 
     return 0;
 }
@@ -66,70 +78,119 @@ int PHAL_I2Cinit(I2C_TypeDef* instance, uint32_t freq, const uint32_t fck) {
     It's late and I want some sleep. If only DoorDash could deliver food on time... :/
 */
 
-int PHAL_I2Cwrite(I2C_TypeDef* instance, uint8_t addr, const uint8_t* data, uint8_t len, bool force) {
+int PHAL_I2C_write_a(I2C_TypeDef* instance, uint8_t addr, const uint8_t* data, uint8_t len, bool stop, bool force) {
     int ret;
     
     // Check if already busy
-    if (core._tx_busy) {
+    if (core->_tx_busy) {
         if (force) {
-            PHAL_stopTxfer(core.tx_dma_cfg);
+            PHAL_stopTxfer(core->tx_dma_cfg);
+            PHAL_I2C_stop(instance);
         } else {
             return -E_BUSY;
         }
     }
 
-    core._tx_busy = true;
+    // Lock core and set stop
+    core->_tx_busy = true;
+    core->stop = stop;
 
     // Configure DMA for TX
-    PHAL_DMA_setTxferLength(core.tx_dma_cfg, len);
-    PHAL_DMA_setMemAddress(core.tx_dma_cfg, (uint32_t) data);
+    PHAL_DMA_setTxferLength(core->tx_dma_cfg, len);
+    PHAL_DMA_setMemAddress(core->tx_dma_cfg, (uint32_t) data);
 
     // Attempt to generate start
     ret = gen_start(instance, addr, len, true);
 
     if (ret < 0) {
-        core._tx_busy = false;
+        core->_tx_busy = false;
 
         return ret;
     }
 
     // Start write
-    PHAL_startTxfer(core.tx_dma_cfg);
+    PHAL_startTxfer(core->tx_dma_cfg);
 
     return 0;
 }
 
-int PHAL_I2Cread(I2C_TypeDef* instance, uint8_t addr, uint8_t* data, uint8_t len, bool force) {
+int PHAL_I2C_read_a(I2C_TypeDef* instance, uint8_t addr, uint8_t* data, uint8_t len, bool stop, bool force) {
     int ret;
 
     // Check if already busy
-    if (core._rx_busy) {
+    if (core->_rx_busy) {
         if (force) {
-            PHAL_stopTxfer(core.rx_dma_cfg);
+            PHAL_stopTxfer(core->rx_dma_cfg);
+            PHAL_I2C_stop(instance);
         } else {
             return -E_BUSY;
         }
     }
 
-    core._rx_busy = true;
+    // Lock core and set stop
+    core->_rx_busy = true;
+    core->stop = stop;
 
     // Configure DMA for RX
-    PHAL_DMA_setTxferLength(core.rx_dma_cfg, len);
-    PHAL_DMA_setMemAddress(core.rx_dma_cfg, (uint32_t) data);
+    PHAL_DMA_setTxferLength(core->rx_dma_cfg, len);
+    PHAL_DMA_setMemAddress(core->rx_dma_cfg, (uint32_t) data);
 
     // Attempt to generate start
     ret = gen_start(instance, addr, len, false);
 
     if (ret < 0) {
-        core._rx_busy = false;
+        core->_rx_busy = false;
 
         return ret;
     }
 
     // Start read
-    PHAL_startTxfer(core.rx_dma_cfg);
+    PHAL_startTxfer(core->rx_dma_cfg);
 
     return 0;
+}
+
+int PHAL_I2C_stop(I2C_TypeDef* instance) {
+    instance->CR2 |= I2C_CR2_STOP;
+    core->_tx_busy = core->_rx_busy = false;
+
+    return 0;
+}
+
+int PHAL_I2C_check_state(void) {
+    return (core->_tx_busy || core->_rx_busy) ? -E_BUSY : 0;
+}
+
+void DMA1_Channel6_IRQHandler()
+{
+    if (DMA1->ISR & DMA_ISR_TEIF6)
+    {
+        DMA1->IFCR |= DMA_IFCR_CTEIF6;
+    }
+    if (DMA1->ISR & DMA_ISR_TCIF6) 
+    {
+        DMA1->IFCR |= DMA_IFCR_CTCIF6;
+    }
+    if (DMA1->ISR & DMA_ISR_GIF6)
+    {
+        DMA1->IFCR |= DMA_IFCR_CGIF6;
+    }
+}
+
+void DMA1_Channel7_IRQHandler()
+{
+    if (DMA1->ISR & DMA_ISR_TEIF7)
+    {
+        DMA1->IFCR |= DMA_IFCR_CTEIF7;
+    }
+    if (DMA1->ISR & DMA_ISR_TCIF7) 
+    {
+        DMA1->IFCR |= DMA_IFCR_CTCIF7;
+    }
+    if (DMA1->ISR & DMA_ISR_GIF7)
+    {
+        DMA1->IFCR |= DMA_IFCR_CGIF7;
+    }
 }
 
 void I2C1_EV_IRQHandler() {
@@ -141,14 +202,54 @@ void I2C3_EV_IRQHandler() {
 }
 
 static void I2C_IRQHandler(I2C_TypeDef* instance) {
+    // Check if bus is busy but we don't think we should be busy
+    if (instance->ISR & I2C_ISR_BUSY && (!core->_tx_busy && !core->_rx_busy)) {
+        core->error[core->err_idx++] = E_BAD_BUSY;
+        core->err_idx = (core->err_idx == MAX_ERR) ? 0 : core->err_idx;
+    }
 
+    // Check if the bus is not busy and unlock
+    if (!(instance->ISR & I2C_ISR_BUSY)) {
+        core->_tx_busy = core->_rx_busy = false;
+    }
+
+    // Check for arbitration loss
+    if (instance->ISR & I2C_ISR_ARLO) {
+        core->error[core->err_idx++] = E_ARLO;
+        core->err_idx = (core->err_idx == MAX_ERR) ? 0 : core->err_idx;
+    }
+
+    // Check for bad start/stop bits
+    if (instance->ISR & I2C_ISR_BERR) {
+        core->error[core->err_idx++] = E_BERR;
+        core->err_idx = (core->err_idx == MAX_ERR) ? 0 : core->err_idx;
+    }
+
+    // Check for slave device NAK
+    if (instance->ISR & I2C_ISR_NACKF) {
+        core->error[core->err_idx++] = E_NACK;
+        core->err_idx = (core->err_idx == MAX_ERR) ? 0 : core->err_idx;
+    }
+
+    // Send a stop if the last byte was just transmitted
+    if (instance->ISR & I2C_ISR_TC) {
+        if (core->stop) {
+            PHAL_I2C_stop(instance);
+        }
+
+        core->_tx_busy = core->_rx_busy = false;
+    }
+
+    // Clear ISR so we don't trigger again from NVIC
+    instance->ICR = 0b11111100111000;
 }
 
 static int gen_start(I2C_TypeDef* instance, uint8_t addr, uint8_t len, bool read) {
     // Clear from last read/write
     instance->CR2 &= 0x07ffffff;
 
-    instance->CR2 |= (len << I2C_CR2_NBYTES_Pos) | (read << I2C_CR2_RD_WRN_Pos) | addr;
+    // Set NBYTES, read/write, and address, then start
+    instance->CR2 |= (len << I2C_CR2_NBYTES_Pos) | (read << I2C_CR2_RD_WRN_Pos) | (addr << 1);
     instance->CR2 |= I2C_CR2_START;
 
     return 0;
@@ -159,10 +260,14 @@ static void i2c_unmask_irq(USART_TypeDef* instance) {
     {
         case I2C1_BASE:
             NVIC->ISER[0] |= 1U << I2C1_EV_IRQn;
+            NVIC->ISER[0] |= 1U << DMA1_Channel6_IRQn;
+            NVIC->ISER[0] |= 1U << DMA1_Channel7_IRQn;
             break;
 
         case I2C3_BASE:
             NVIC->ISER[2] |= 1U << (I2C3_EV_IRQn - 64);
+            NVIC->ISER[0] |= 1U << DMA1_Channel2_IRQn;
+            NVIC->ISER[0] |= 1U << DMA1_Channel3_IRQn;
             break;
     }
 }
@@ -172,10 +277,14 @@ static void i2c_mask_irq(USART_TypeDef* instance) {
     {
         case I2C1_BASE:
             NVIC->ISER[0] &= ~(1U << I2C1_EV_IRQn);
+            NVIC->ISER[0] &= ~(1U << DMA1_Channel6_IRQn);
+            NVIC->ISER[0] &= ~(1U << DMA1_Channel7_IRQn);
             break;
 
         case I2C3_BASE:
             NVIC->ISER[2] &= ~(1U << (I2C3_EV_IRQn - 64));
+            NVIC->ISER[0] &= ~(1U << DMA1_Channel2_IRQn);
+            NVIC->ISER[0] &= ~(1U << DMA1_Channel3_IRQn);
             break;
     }
 }
--- common/phal_L4/i2c_alt/i2c_alt.h
+++ common/phal_L4/i2c_alt/i2c_alt.h
@@ -15,12 +15,16 @@ enum {
     E_CONFIG = 1,                   // Improper config error
     E_NACK,                         // NACK when shouldn't see a NACK
     E_BUSY,                         // I2C busy
-    E_NO_START                      // Start not generated
+    E_NO_START,                     // Start not generated
+    E_BAD_BUSY,                     // I2C reads busy, but we don't think we should be
+    E_ARLO,                         // Arbitration lost
+    E_BERR                          // Misplaced start/stop detected
 };
 
 // Structures
 typedef struct {
     int         error[MAX_ERR];     // Previous and current error codes
+    bool        stop;               // Stop bit after TX completion
     uint8_t     err_idx;            // Current error index
 
     // DMA configurations
@@ -29,44 +33,45 @@ typedef struct {
 
     uint8_t     _tx_busy;           // Waiting on a transmission to finish
     uint8_t     _rx_busy;           // Waiting on a reception to finish
-} i2c_core_t;
+} i2c_handle_t;
 
 // Prototypes
-int PHAL_initI2C_a(I2C_TypeDef* instance, uint32_t freq, const uint32_t fck);
-int PHAL_I2Cwrite(I2C_TypeDef* instance, uint8_t addr, const uint8_t* data, uint8_t len, bool force);
-int PHAL_I2Cread(I2C_TypeDef* instance, uint8_t addr, uint8_t* data, uint8_t len, bool force);
+int PHAL_I2C_init(I2C_TypeDef* instance, i2c_handle_t* handle, uint32_t freq, const uint32_t fck);
+int PHAL_I2C_write_a(I2C_TypeDef* instance, uint8_t addr, const uint8_t* data, uint8_t len, bool stop, bool force);
+int PHAL_I2C_read_a(I2C_TypeDef* instance, uint8_t addr, uint8_t* data, uint8_t len, bool stop, bool force);
+int PHAL_I2C_stop(I2C_TypeDef* instance);
+int PHAL_I2C_check_state(void);
 
-// TODO: Fix channels to actually align with what I2C wants
-#define I2C1_TXDMA_CONT_CONFIG(tx_addr_, priority_)                           \
-    {.periph_addr=(uint32_t) &(I2C1->TXDR), .mem_addr=(uint32_t) (tx_addr_),   \
+#define I2C1_TXDMA_CONT_CONFIG(tx_addr_, priority_)                             \
+    {.periph_addr=(uint32_t) &(I2C1->TXDR), .mem_addr=(uint32_t) (tx_addr_),    \
      .tx_size=1, .increment=false, .circular=false,                             \
      .dir=0b1, .mem_inc=true, .periph_inc=false, .mem_to_mem=false,             \
      .priority=(priority_), .mem_size=0b00, .periph_size=0b00,                  \
-     .tx_isr_en=true, .dma_chan_request=0b0010, .channel_idx=4,                \
-     .periph=DMA1, .channel=DMA1_Channel4, .request=DMA1_CSELR}
+     .tx_isr_en=true, .dma_chan_request=0b0011, .channel_idx=6,                 \
+     .periph=DMA1, .channel=DMA1_Channel6, .request=DMA1_CSELR}
 
-#define I2C1_RXDMA_CONT_CONFIG(rx_addr_, priority_)                           \
-    {.periph_addr=(uint32_t) &(I2C1->RXDR), .mem_addr=(uint32_t) (rx_addr_),   \
+#define I2C1_RXDMA_CONT_CONFIG(rx_addr_, priority_)                             \
+    {.periph_addr=(uint32_t) &(I2C1->RXDR), .mem_addr=(uint32_t) (rx_addr_),    \
      .tx_size=1, .increment=false, .circular=false,                             \
      .dir=0b0, .mem_inc=true, .periph_inc=false, .mem_to_mem=false,             \
      .priority=(priority_), .mem_size=0b00, .periph_size=0b00,                  \
-     .tx_isr_en=true, .dma_chan_request=0b0010, .channel_idx=5,                 \
-     .periph=DMA1, .channel=DMA1_Channel5, .request=DMA1_CSELR}
+     .tx_isr_en=true, .dma_chan_request=0b0011, .channel_idx=7,                 \
+     .periph=DMA1, .channel=DMA1_Channel7, .request=DMA1_CSELR}
 
-#define I2C3_TXDMA_CONT_CONFIG(tx_addr_, priority_)                           \
-    {.periph_addr=(uint32_t) &(I2C3->TXDR), .mem_addr=(uint32_t) (tx_addr_),   \
+#define I2C3_TXDMA_CONT_CONFIG(tx_addr_, priority_)                             \
+    {.periph_addr=(uint32_t) &(I2C3->TXDR), .mem_addr=(uint32_t) (tx_addr_),    \
      .tx_size=1, .increment=false, .circular=false,                             \
      .dir=0b1, .mem_inc=true, .periph_inc=false, .mem_to_mem=false,             \
      .priority=(priority_), .mem_size=0b00, .periph_size=0b00,                  \
-     .tx_isr_en=true, .dma_chan_request=0b0010, .channel_idx=4,                \
-     .periph=DMA1, .channel=DMA1_Channel4, .request=DMA1_CSELR}
+     .tx_isr_en=true, .dma_chan_request=0b0011, .channel_idx=2,                 \
+     .periph=DMA1, .channel=DMA1_Channel2, .request=DMA1_CSELR}
 
-#define I2C3_RXDMA_CONT_CONFIG(rx_addr_, priority_)                           \
-    {.periph_addr=(uint32_t) &(I2C3->RXDR), .mem_addr=(uint32_t) (rx_addr_),   \
+#define I2C3_RXDMA_CONT_CONFIG(rx_addr_, priority_)                             \
+    {.periph_addr=(uint32_t) &(I2C3->RXDR), .mem_addr=(uint32_t) (rx_addr_),    \
      .tx_size=1, .increment=false, .circular=false,                             \
      .dir=0b0, .mem_inc=true, .periph_inc=false, .mem_to_mem=false,             \
      .priority=(priority_), .mem_size=0b00, .periph_size=0b00,                  \
-     .tx_isr_en=true, .dma_chan_request=0b0010, .channel_idx=5,                 \
-     .periph=DMA1, .channel=DMA1_Channel5, .request=DMA1_CSELR}
+     .tx_isr_en=true, .dma_chan_request=0b0011, .channel_idx=3,                 \
+     .periph=DMA1, .channel=DMA1_Channel3, .request=DMA1_CSELR}
 
 #endif

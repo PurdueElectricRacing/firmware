3b6f9ac584f05f0dc4f0610b044698e7c2f0113a
R-A-I-D-er <shuiy@purdue.edu>
2022-05-14 12:29:32 -0400

    pure serial port output fixed on 5V internal ADC reference

--- source/thermistor/thermistor.ino
+++ source/thermistor/thermistor.ino
@@ -16,15 +16,17 @@
 
 void setup()
 {
+  //analogReference(EXTERNAL);
+  
 	pinMode(ADC_MUX_ADDR_0_PIN, OUTPUT);
 	pinMode(ADC_MUX_ADDR_1_PIN, OUTPUT);
 	pinMode(ADC_MUX_ADDR_2_PIN, OUTPUT);
 	pinMode(ADC_MUX_ADDR_3_PIN, OUTPUT);
 	
-	pinMode(ADC_MUX_SIG_0_PIN, 	INPUT_PULLUP);
-	pinMode(ADC_MUX_SIG_1_PIN, 	INPUT_PULLUP);
-	pinMode(ADC_MUX_SIG_2_PIN, 	INPUT_PULLUP);
-	pinMode(ADC_MUX_SIG_3_PIN, 	INPUT_PULLUP);
+//	pinMode(ADC_MUX_SIG_0_PIN);
+//	pinMode(ADC_MUX_SIG_1_PIN);
+//	pinMode(ADC_MUX_SIG_2_PIN);
+//	pinMode(ADC_MUX_SIG_3_PIN);
 
 	Serial.begin(9600);
 	while (!Serial);
@@ -33,7 +35,7 @@ void setup()
 
 	if (!CAN.begin(500E3)) {
 		Serial.println("Error with CAN initilization!");
-		while (1);
+		//while (1);
 	}
 }
 
@@ -42,10 +44,10 @@ void loop()
 	static uint8_t mux_index = 0;
 
 	// MUX decoding
-	digitalWrite(ADC_MUX_SIG_0_PIN, (mux_index & 0b0001) == 0b0001);
-	digitalWrite(ADC_MUX_SIG_1_PIN, (mux_index & 0b0010) == 0b0010);
-	digitalWrite(ADC_MUX_SIG_2_PIN, (mux_index & 0b0100) == 0b0100);
-	digitalWrite(ADC_MUX_SIG_3_PIN, (mux_index & 0b1000) == 0b1000);
+	digitalWrite(ADC_MUX_ADDR_0_PIN, (mux_index & 0b0001) == 0b0001);
+	digitalWrite(ADC_MUX_ADDR_1_PIN, (mux_index & 0b0010) == 0b0010);
+	digitalWrite(ADC_MUX_ADDR_2_PIN, (mux_index & 0b0100) == 0b0100);
+	digitalWrite(ADC_MUX_ADDR_3_PIN, (mux_index & 0b1000) == 0b1000);
 	
 	// Allow for analog mux to settle, 
 	delay(250);
@@ -58,6 +60,21 @@ void loop()
 
 	// CAN message construction
 	uint16_t msg_id = CAN_MESSAGE_ID_BASE + mux_index;
+
+  if(temp0 < 0 || temp0 > 1000){
+      Serial.print("--- mux_index = ");
+      Serial.print(mux_index);
+      Serial.print(", temp0 (raw) = ");
+      Serial.print(temp0);
+      Serial.print("--- \n");
+    } else {
+      Serial.print("mux_index = ");
+      Serial.print(mux_index);
+      Serial.print(", temp0 (raw) = ");
+      Serial.print(temp0);
+      Serial.print("\n");     
+    }
+   
 	CAN.beginExtendedPacket(msg_id);
 	CAN.write((temp0 >> 8) & 0xFF);
 	CAN.write((temp0) & 0xFF);
@@ -69,5 +86,10 @@ void loop()
 	CAN.write((temp3) & 0xFF);
 	CAN.endPacket();
 
-	mux_index++;
+  if(mux_index == 15){
+    mux_index = 0;
+  } else {
+    mux_index++;
+  }
+
 }

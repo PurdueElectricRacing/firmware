e49137d9c389d9d1365f882bf0f25283926ee899
AdityaAsGithub <AdityaAsGithub@users.noreply.github.com>
2022-11-05 16:01:51 -0400

    Adding Basic struct defs

--- common/faults/faults.c
+++ common/faults/faults.c
@@ -1,1 +1,20 @@
 #include "faults.h"
+1
+
+bool fault_lib_enable;
+
+bool setFault(fault_struct_t *faultToFind, int valueToCompare) {
+    if (!fault_lib_enable)
+    {
+
+    }
+    else return false;
+}
+
+void killFaultLibrary() {
+    fault_lib_enable = false;
+}
+
+void initFaultLibrary() {
+    fault_lib_enable = true;
+}

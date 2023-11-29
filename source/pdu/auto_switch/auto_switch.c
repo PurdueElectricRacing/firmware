630a9edad10fbf7c942ec1e9bde50715ea06c50e
Gavin Zyonse <gavin@zyonse.com>
2023-11-29 16:31:17 -0500

    Initial current and voltage functions

---
+++ source/pdu/auto_switch/auto_switch.c
@@ -0,0 +1,66 @@
+/**
+ * @file auto_switch.c
+ * @author Gavin Zyonse (gzyonse@purdue.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-09
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "auto_switch.h"
+
+uint8_t faultStatus() {
+    return 0;
+}
+
+void getCurrent() {
+    // High power switches
+    auto_switch.current[SW_PUMP_1] = getCurrent_HP(adc_readings.pump_1_imon);
+    auto_switch.current[SW_PUMP_2] = getCurrent_HP(adc_readings.pump_2_imon);
+    auto_switch.current[SW_SDC] = getCurrent_HP(adc_readings.sdc_imon);
+    auto_switch.current[SW_AUX] = getCurrent_HP(adc_readings.aux_hp_imon);
+
+    // Low power switches
+    auto_switch.current[SW_FAN_1] = getCurrent_LP(adc_readings.fan_1_cs);
+    auto_switch.current[SW_FAN_2] = getCurrent_LP(adc_readings.fan_2_cs);
+    auto_switch.current[SW_DASH] = getCurrent_LP(adc_readings.dash_cs);
+    auto_switch.current[SW_ABOX] = getCurrent_LP(adc_readings.abox_cs);
+    auto_switch.current[SW_MAIN] = getCurrent_LP(adc_readings.main_cs);
+    // TODO: bullet switch doesn't have cs signal
+
+    // 5V switches
+    /*
+    auto_switch.current[SW_CRIT_5V] =;
+    auto_switch.current[SW_NCRIT_5V] =;
+    auto_switch.current[SW_DAQ] =;
+    auto_switch.current[SW_FAN_5V] =;
+    */
+}
+
+// Updates voltage for all switches and stores values in struct
+void getVoltage() {
+    auto_switch.voltage.in_24v = adc_readings.lv_24_v_sense * (3300 / (47000 + 3300));
+    auto_switch.voltage.out_5v = adc_readings.lv_5_v_sense * (3300 / (4300 + 3300));
+    auto_switch.voltage.out_3v3 = adc_readings.lv_3v3_v_sense * (10000 / (4300 + 10000));
+}
+
+// Current helper functions
+float getCurrent_HP(uint16_t adc_reading) {
+    uint16_t current = adc_reading;
+    return current;
+}
+
+float getCurrent_LP(uint16_t adc_reading) {
+    // There's probably some math to be done here
+    uint16_t current = adc_reading;
+    return current;
+}
+
+float getCurrent_5V() {
+    return 0;
+}
+
+void enableSwitch() {
+
+}
---
+++ source/pdu/auto_switch/auto_switch.h
@@ -0,0 +1,64 @@
+/**
+ * @file auto_switch.h
+ * @author Gavin Zyonse (gzyonse@purdue.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-09
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef _AUTO_SWITCH_H_
+#define _AUTO_SWITCH_H_
+
+#include <stdint.h>
+#include <source/pdu/main.h>
+
+// Static variables
+#define SHUNT_R 10000000
+
+// Enumeration
+typedef enum {
+    // High power switches
+    SW_PUMP_1,
+    SW_PUMP_2,
+    SW_SDC,
+    SW_AUX,
+    // Low power switches
+    SW_FAN_1,
+    SW_FAN_2,
+    SW_DASH,
+    SW_ABOX,
+    SW_MAIN,
+    SW_BLT,
+    // 5V switches
+    SW_CRIT_5V,
+    SW_NCRIT_5V,
+    SW_DAQ,
+    SW_FAN_5V,
+
+    // Number of switches
+    NUM_SWITCHES
+} switches_t;
+
+// Structures
+typedef struct {
+    float in_24v;
+    float out_5v;
+    float out_3v3;
+} voltage_t;
+
+typedef struct {
+    float current[NUM_SWITCHES];
+    voltage_t voltage;
+} auto_switch_t;
+
+extern auto_switch_t auto_switch;
+
+// Function definitions
+uint8_t faultStatus();
+void getCurrent();
+void getVoltage();
+void enableSwitch();
+
+#endif

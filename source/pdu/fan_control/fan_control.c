c3868ac985a304e6ce614b682eb336756d9c5e00
Cole <cole@colex.dev>
2024-01-25 20:39:59 -0500

    Starting tach

--- source/pdu/fan_control/fan_control.c
+++ source/pdu/fan_control/fan_control.c
@@ -35,27 +35,42 @@ bool fanControlInit()
     // NOTE: Set up regs so that CCR1 can be directly set from 0-100
     FAN_PWM_TIM -> ARR = 100 - 1; // Using this for ease of calculations
 
-    /* NOTE: 75 is not off, just testing */
-    FAN_PWM_TIM -> CCR1 = 75; // Start with it off
+    FAN_PWM_TIM -> CCR1 = 0; // Start with it off
+    FAN_PWM_TIM -> CCR2 = 0; // Start with it off
 
     // Set PWM mode 1 (active while CNT <= CCR1)
     FAN_PWM_TIM -> CCMR1 &= ~TIM_CCMR1_OC1M;
-    FAN_PWM_TIM -> CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1; // PWM Mode 1
-    FAN_PWM_TIM -> CCMR1 |= TIM_CCMR1_OC1PE; // Enable preload register
+    FAN_PWM_TIM -> CCMR1 &= ~TIM_CCMR1_OC2M;
+    FAN_PWM_TIM -> CCMR1 |= TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1; // PWM Mode 1 ch1
+    FAN_PWM_TIM -> CCMR1 |= TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1; // PWM Mode 1 ch2
+    FAN_PWM_TIM -> CCMR1 |= TIM_CCMR1_OC2PE |TIM_CCMR1_OC1PE; // Enable preload register ch1 & ch2
 
-    FAN_PWM_TIM -> CCER |= TIM_CCER_CC1E; // Enable output compare 1
+    FAN_PWM_TIM -> CCER |= TIM_CCER_CC2E | TIM_CCER_CC1E; // Enable output compare 1 & 2 
+    // do I even need 2?
     FAN_PWM_TIM -> BDTR |= TIM_BDTR_MOE; // Enable main output
 
     FAN_PWM_TIM -> CR1 &= ~TIM_CR1_DIR; // Set to upcounting mode
     FAN_PWM_TIM -> CR1 |= TIM_CR1_ARPE; // Necessary in upcounting mode
 
-    // FAN_PWM_TIM -> EGR |= TIM_EGR_UG;
-
     FAN_PWM_TIM -> CR1 |= TIM_CR1_CEN; // Enable counter (turn on timer)
 
+    // Enable FAN_PWM_TIM
+    FAN_PWM_TIM->CR1 |= TIM_CR1_CEN;    
 
-    // Enable TIM1
-    TIM1->CR1 |= TIM_CR1_CEN;    
+    /* FAN_1_TACH_TIM */
+    RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+    FAN_1_TACH_TIM -> CR1 &= ~TIM_CR1_CEN; // Disable counter (turn off timer)
+
+    /* Set input capture mode */
+    FAN_1_TACH_TIM -> CCER &= ~TIM_CCER_CC2E; // Turn of the channel (necessary to write CC2S bits)
+    FAN_1_TACH_TIM -> CCMR1 &= ~TIM_CCMR1_CC2S;
+    FAN_1_TACH_TIM -> CCMR1 |= TIM_CCMR1_CC2S_0; // Map IC2 to TI2
+
+    FAN_1_TACH_TIM -> CCER &= ~TIM_CCER_CC2P; // Capture input on the rising edge
+    FAN_1_TACH_TIM -> CCER |= TIM_CCER_CC2E; // Enable the channel
+
+    FAN_1_TACH_TIM -> CR1 |= TIM_CR1_CEN; // Enable timer
+
     return true;
 }
 

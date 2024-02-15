6b9293ff6ab1ab6f6aa97efb29cb9e4acab1e189
Cole <cole@colex.dev>
2024-02-15 08:48:52 -0500

    Cooling fan PWM control (#92)
    * Starting fan timer init
    
    * Pushing init function
    
    * Starting tach
    
    * Setting fan speed is working, tach is not
    
    * Messing around with prescalar and auto reload
    
    * Second fan tach
    
    * I think I have the tach working now
    
    * RPM calculation should now be correct
    
    * Pushing power settings
    
    * Adding extra functions
    
    * Only setting up PWM control for PR, no tach

---
+++ source/pdu/fan_control/fan_control.h
@@ -0,0 +1,15 @@
+#ifndef _FAN_CONTROL_H_
+#define _FAN_CONTROL_H_
+
+#include <stdbool.h>
+#include "stm32f407xx.h"
+#include "main.h"
+
+#define PWM_FREQUENCY_HZ (25000) // PWM frequency to be 25kHz
+#define FAN_PWM_TIM (FAN_1_PWM_TIM) // Fan 1 and 2 user same timer
+
+bool fanControlInit();
+void setFan1Speed(uint8_t fan_speed);
+void setFan2Speed(uint8_t fan_speed);
+
+#endif // _FAN_CONTROL_H_

1c2f53ea02b96dd6b8c03f46564ef11162fcb6aa
Cole <cole@colex.dev>
2024-02-29 09:34:10 -0500

    I think this is correct, I just need to test it later

--- source/pdu/flow_rate/flow_rate.c
+++ source/pdu/flow_rate/flow_rate.c
@@ -8,50 +8,51 @@ extern uint32_t APB2ClockRateHz;
 
 bool flowRateInit()
 {
+    /* NOTE: This is just here for testing reading in a PWM signal */
     fanControlInit();
     setFan1Speed(50);
 
     /* FLOW_RATE_1_TIM */
     // CH1
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
-    FAN_2_TACH_TIM->CR1 &= ~TIM_CR1_CEN; // Disable counter (turn off timer)
+    FLOW_RATE_1_TIM->CR1 &= ~TIM_CR1_CEN; // Disable counter (turn off timer)
 
-    FAN_2_TACH_TIM->PSC = 1 - 1;
-    FAN_2_TACH_TIM->ARR = 0xFFFF - 1;
+    FLOW_RATE_1_TIM->PSC = 1 - 1;
+    FLOW_RATE_1_TIM->ARR = 0xFFFF - 1;
 
     /* Set input capture mode */
-    FAN_2_TACH_TIM->CCER &= ~TIM_CCER_CC1E; // Turn off the channel (necessary to write CC1S bits)
-    FAN_2_TACH_TIM->CCER &= ~TIM_CCER_CC2E; // Turn off the channel (necessary to write CC2S bits)
+    FLOW_RATE_1_TIM->CCER &= ~TIM_CCER_CC1E; // Turn off the channel (necessary to write CC1S bits)
+    FLOW_RATE_1_TIM->CCER &= ~TIM_CCER_CC2E; // Turn off the channel (necessary to write CC2S bits)
 
     /* Setup capture compare 1 (period) */
-    FAN_2_TACH_TIM->CCMR1 &= ~TIM_CCMR1_CC1S;
-    FAN_2_TACH_TIM->CCMR1 |= TIM_CCMR1_CC1S_1; // Map IC1 to TI2
+    FLOW_RATE_1_TIM->CCMR1 &= ~TIM_CCMR1_CC1S;
+    FLOW_RATE_1_TIM->CCMR1 |= TIM_CCMR1_CC1S_0; // Map IC1 to TI1
 
     /* Setup capture compare 2 (duty cycle) */
-    FAN_2_TACH_TIM->CCMR1 &= ~TIM_CCMR1_CC2S;
-    FAN_2_TACH_TIM->CCMR1 |= TIM_CCMR1_CC2S_0; // Map IC2 to TI2
+    FLOW_RATE_1_TIM->CCMR1 &= ~TIM_CCMR1_CC2S;
+    FLOW_RATE_1_TIM->CCMR1 |= TIM_CCMR1_CC2S_1; // Map IC2 to TI1
 
     /* CCR1 (period) needs rising edge */
-    FAN_2_TACH_TIM->CCER &= ~TIM_CCER_CC1P;
-    FAN_2_TACH_TIM->CCER &= ~TIM_CCER_CC1NP;
+    FLOW_RATE_1_TIM->CCER &= ~TIM_CCER_CC1P;
+    FLOW_RATE_1_TIM->CCER &= ~TIM_CCER_CC1NP;
 
     /* CCR2 (duty cycle) needs falling edge */
-    FAN_2_TACH_TIM->CCER |= TIM_CCER_CC1P;
-    FAN_2_TACH_TIM->CCER &= ~TIM_CCER_CC1NP;
+    FLOW_RATE_1_TIM->CCER |= TIM_CCER_CC2P;
+    FLOW_RATE_1_TIM->CCER &= ~TIM_CCER_CC2NP;
 
     /* Select trigger */
-    FAN_2_TACH_TIM->SMCR &= ~TIM_SMCR_TS;
-    FAN_2_TACH_TIM->SMCR |= TIM_SMCR_TS_2 | TIM_SMCR_TS_1;
+    FLOW_RATE_1_TIM->SMCR &= ~TIM_SMCR_TS;
+    FLOW_RATE_1_TIM->SMCR |= TIM_SMCR_TS_2 | TIM_SMCR_TS_0;
 
     /* Select trigger */
-    FAN_2_TACH_TIM->SMCR &= ~TIM_SMCR_SMS;
-    FAN_2_TACH_TIM->SMCR |= TIM_SMCR_SMS_2;
+    FLOW_RATE_1_TIM->SMCR &= ~TIM_SMCR_SMS;
+    FLOW_RATE_1_TIM->SMCR |= TIM_SMCR_SMS_2;
 
     /* Enable channels */
-    FAN_2_TACH_TIM->CCER |= TIM_CCER_CC1E; // Enable CCR1
-    FAN_2_TACH_TIM->CCER |= TIM_CCER_CC2E; // Enable CCR2
+    FLOW_RATE_1_TIM->CCER |= TIM_CCER_CC1E; // Enable CCR1
+    FLOW_RATE_1_TIM->CCER |= TIM_CCER_CC2E; // Enable CCR2
 
-    FAN_2_TACH_TIM->CR1 |= TIM_CR1_CEN; // Enable timer
+    FLOW_RATE_1_TIM->CR1 |= TIM_CR1_CEN; // Enable timer
 
     /* FLOW_RATE_2_TIM */
     // CH2
@@ -79,8 +80,8 @@ bool flowRateInit()
     FLOW_RATE_2_TIM->CCER &= ~TIM_CCER_CC1NP;
 
     /* CCR2 (duty cycle) needs falling edge */
-    FLOW_RATE_2_TIM->CCER |= TIM_CCER_CC1P;
-    FLOW_RATE_2_TIM->CCER &= ~TIM_CCER_CC1NP;
+    FLOW_RATE_2_TIM->CCER |= TIM_CCER_CC2P;
+    FLOW_RATE_2_TIM->CCER &= ~TIM_CCER_CC2NP;
 
     /* Select trigger */
     FLOW_RATE_2_TIM->SMCR &= ~TIM_SMCR_TS;

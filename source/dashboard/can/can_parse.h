9c4aa8ba18bcb59b94fa455021cb8f27275cb9bb
AdityaAsGithub <AdityaAsGithub@users.noreply.github.com>
2024-03-23 15:39:31 -0400

    stale checks

--- source/dashboard/can/can_parse.h
+++ source/dashboard/can/can_parse.h
@@ -182,7 +182,7 @@
 /* END AUTO SEND MACROS */
 
 // Stale Checking
-#define STALE_THRESH 3 / 2 // 3 / 2 would be 150% of period
+#define STALE_THRESH 5 / 2 // 3 / 2 would be 150% of period
 /* BEGIN AUTO UP DEFS (Update Period)*/
 #define UP_MAIN_HB 100
 #define UP_REAR_MOTOR_CURRENTS_TEMPS 500

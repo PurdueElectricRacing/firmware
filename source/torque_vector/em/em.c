bdfe486248e7552d051f31e8b1c2d4e08d8f163c
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-04-06 13:34:17 -0400

    update engine map tables

--- source/torque_vector/em/em.c
+++ source/torque_vector/em/em.c
@@ -7,9 +7,9 @@
  *
  * Code generated for Simulink model 'em'.
  *
- * Model version                  : 1.18
+ * Model version                  : 1.20
  * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
- * C/C++ source code generated on : Tue Mar 12 19:10:03 2024
+ * C/C++ source code generated on : Sat Apr  6 13:25:04 2024
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex-M

fc0e592f8e646317b86eee6e2f8ac30fbb29d109
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-04-12 12:45:47 -0400

    re-enable can messages for debugging

--- source/torque_vector/imu/imu.c
+++ source/torque_vector/imu/imu.c
@@ -55,7 +55,7 @@ void imu_periodic(IMU_Handle_t *imu_h)
     r = (int16_t)(imu_h->output.orientation.roll * 10.0f / DEG_TO_RAD);
     y = (int16_t)(imu_h->output.orientation.yaw * 10.0f / DEG_TO_RAD);
 
-    // SEND_IMU_GYRO((int16_t)(gyro_in.x * 100), (int16_t)(gyro_in.y * 100), (int16_t)(gyro_in.z * 100));
-    // SEND_IMU_ACCEL((int16_t)(accel_in.x * 100), (int16_t)(accel_in.y * 100), (int16_t)(accel_in.z * 100));
+    SEND_IMU_GYRO((int16_t)(gyro_in.x * 100), (int16_t)(gyro_in.y * 100), (int16_t)(gyro_in.z * 100));
+    SEND_IMU_ACCEL((int16_t)(accel_in.x * 100), (int16_t)(accel_in.y * 100), (int16_t)(accel_in.z * 100));
     t_us += 10000;
 }

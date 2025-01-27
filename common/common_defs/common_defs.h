3e15b1f074d1373d52842eebb79900d28a4ffcfc
AdityaAsGithub <AdityaAsGithub@users.noreply.github.com>
2025-01-27 16:45:52 -0500

    CCAN 250k

--- common/common_defs/common_defs.h
+++ common/common_defs/common_defs.h
@@ -46,7 +46,7 @@
 #define VCAN_BPS 500000 // Main vehicle CAN bus
 #define DCAN_BPS 500000 // Steering angle, DAQ, (wheel temps)
 #define MCAN_BPS 500000 // Motor CAN Bus (Main, DAQ, Inverters)
-#define CCAN_BPS 500000 // Charger CAN bus (ELCON, A_Box, Orion)
+#define CCAN_BPS 250000 // Charger CAN bus (ELCON, A_Box, Orion)
 
 /* Per-Node HSI RCC Trim Constants */
 #define HSI_TRIM_TORQUE_VECTOR 	15

81c0c17014dbf175c34d9c29f3e2af6f8bc7a2ad
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2025-02-08 08:21:03 -0500

    remove engine map

--- source/torque_vector/em_pp/em_pp.h
+++
@@ -1,4 +0,0 @@
-#include "em.h"
-#include "tv.h"
-
-void em_pp(ExtU_em* rtU_tm, ExtY_tv* rtY_tv);
--- source/torque_vector/main.c
+++ source/torque_vector/main.c
@@ -18,9 +18,6 @@
 #include "imu.h"
 #include "gps.h"
 
-#include "em.h"
-#include "em_pp.h"
-
 #include "tv.h"
 #include "tv_pp.h"
 
@@ -29,9 +26,6 @@
 #include "bmi088.h"
 #include "imu.h"
 #include "gps.h"
-
-#include "em.h"
-#include "em_pp.h"
 
 #include "tv.h"
 #include "tv_pp.h"
@@ -159,15 +153,6 @@ static RT_MODEL_tv *const rtMPtr_tv = &rtM_tvv; /* Real-time model */
 static DW_tv rtDW_tv;                        /* Observable states */
 static RT_MODEL_tv *const rtM_tv = rtMPtr_tv;
 static int16_t tv_timing;
-
-/* Engine Map Definitions */
-static ExtU_em rtU_em; /* External inputs */
-static ExtY_em rtY_em; /* External outputs */
-static RT_MODEL_em rtM_emv;
-static RT_MODEL_em *const rtMPtr_em = &rtM_emv; /* Real-time model */
-static DW_em rtDW_em;                        /* Observable states */
-static RT_MODEL_em *const rtM_em = rtMPtr_em;
-static int16_t em_timing;
 
 /* Moving Median Definition */
 static int16_t gyro_counter = 0; /* Number of steps that gyro has not been checked */
@@ -379,14 +364,6 @@ void VCU_MAIN(void)
     tv_timing = sched.os_ticks;
     tv_step(rtMPtr_tv, &rtU_tv, &rtY_tv);
     tv_timing = sched.os_ticks - tv_timing;
-
-    /* Populate engine map inputs */
-    em_pp(&rtU_em, &rtY_tv);
-
-    /* Step engine map */
-    em_timing = sched.os_ticks;
-    em_step(rtMPtr_em, &rtU_em, &rtY_em);
-    em_timing = sched.os_ticks - em_timing;
 
     /* Set TV faults */
     setFault(ID_TV_DISABLED_FAULT,!rtY_tv.TVS_STATE);

90ef80bee29ead9426c092e93e9a86b70ffc8e20
Chris McGalliard <cpmcgalliard@gmail.com>
2025-03-08 12:16:49 -0500

    blank TV for CAN

--- source/torque_vector/main.c
+++ source/torque_vector/main.c
@@ -12,36 +12,6 @@
 #include "main.h"
 #include "source/torque_vector/can/can_parse.h"
 
-#include "bsxlite_interface.h"
-
-#include "bmi088.h"
-#include "imu.h"
-#include "gps.h"
-
-#include "ac_ext.h"
-#include "ac_compute_R.h"
-
-#include "em.h"
-#include "em_pp.h"
-
-#include "tv.h"
-#include "tv_pp.h"
-
-#include "bsxlite_interface.h"
-
-#include "bmi088.h"
-#include "imu.h"
-#include "gps.h"
-
-#include "ac_ext.h"
-#include "ac_compute_R.h"
-
-#include "em.h"
-#include "em_pp.h"
-
-#include "tv.h"
-#include "tv_pp.h"
-
 uint8_t collect_test[100] = {0};
 
 GPIOInitConfig_t gpio_config[] = {
@@ -50,29 +20,10 @@ GPIOInitConfig_t gpio_config[] = {
     GPIO_INIT_OUTPUT(CONN_LED_GPIO_Port, CONN_LED_Pin, GPIO_OUTPUT_LOW_SPEED),
     GPIO_INIT_OUTPUT(HEARTBEAT_GPIO_Port, HEARTBEAT_Pin, GPIO_OUTPUT_LOW_SPEED),
 
-    // SPI
-    GPIO_INIT_AF(SPI_SCLK_GPIO_Port, SPI_SCLK_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MOSI_GPIO_Port, SPI_MOSI_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN),
-    GPIO_INIT_AF(SPI_MISO_GPIO_Port, SPI_MISO_Pin, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN),
-    GPIO_INIT_OUTPUT(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, GPIO_OUTPUT_HIGH_SPEED),
-    GPIO_INIT_OUTPUT(SPI_CS_GYRO_GPIO_Port, SPI_CS_GYRO_Pin, GPIO_OUTPUT_HIGH_SPEED),
-    GPIO_INIT_OUTPUT(SPI_CS_MAG_GPIO_Port, SPI_CS_MAG_Pin, GPIO_OUTPUT_HIGH_SPEED),
-
-    // GPS USART
-    GPIO_INIT_UART4RX_PA1,
-    GPIO_INIT_UART4TX_PA0,
-
-    // GPS Auxillary pins
-    GPIO_INIT_OUTPUT(GPS_RESET_GPIO_Port, GPS_RESET_Pin, GPIO_OUTPUT_LOW_SPEED),
-
-    // EEPROM
-    GPIO_INIT_OUTPUT(NAV_EEPROM_CS_GPIO_PORT, NAV_EEPROM_CS_PIN, GPIO_OUTPUT_HIGH_SPEED),
-    GPIO_INIT_OUTPUT(NAV_WP_GPIO_PORT, NAV_WP_PIN, GPIO_OUTPUT_HIGH_SPEED),
-
     // CAN
     GPIO_INIT_CANRX_PA11,
     GPIO_INIT_CANTX_PA12
-    };
+};
 
 // GPS USART Configuration
 dma_init_t usart_gps_tx_dma_config = USART4_TXDMA_CONT_CONFIG(NULL, 1);
@@ -113,73 +64,11 @@ extern uint32_t APB2ClockRateHz;
 extern uint32_t AHBClockRateHz;
 extern uint32_t PLLClockRateHz;
 
-dma_init_t spi_rx_dma_config = SPI1_RXDMA_CONT_CONFIG(NULL, 2);
-dma_init_t spi_tx_dma_config = SPI1_TXDMA_CONT_CONFIG(NULL, 1);
-
-SPI_InitConfig_t spi_config = {
-    .data_rate = TargetCoreClockrateHz / 64,
-    .data_len = 8,
-    .nss_sw = true,
-    .nss_gpio_port = SPI_CS_ACEL_GPIO_Port,
-    .nss_gpio_pin = SPI_CS_ACEL_Pin,
-    .rx_dma_cfg = &spi_rx_dma_config,
-    .tx_dma_cfg = &spi_tx_dma_config,
-    .periph = SPI1};
-
-// Test Nav Message
-GPS_Handle_t GPSHandle = {};
-vector_3d_t accel_in, gyro_in, mag_in;
-
-
-BMI088_Handle_t bmi_config = {
-    .accel_csb_gpio_port = SPI_CS_ACEL_GPIO_Port,
-    .accel_csb_pin = SPI_CS_ACEL_Pin,
-    .accel_range = ACCEL_RANGE_3G,
-    .accel_odr = ACCEL_ODR_50Hz,
-    .accel_bwp = ACCEL_OS_NORMAL,
-    .gyro_csb_gpio_port = SPI_CS_GYRO_GPIO_Port,
-    .gyro_csb_pin = SPI_CS_GYRO_Pin,
-    .gyro_datarate = GYRO_DR_100Hz_32Hz,
-    .gyro_range = GYRO_RANGE_250,
-    .spi = &spi_config};
-
-IMU_Handle_t imu_h = {
-    .bmi = &bmi_config,
-};
-
 /* Function Prototypes */
 void heartBeatLED(void);
 void preflightAnimation(void);
 void preflightChecks(void);
 extern void HardFault_Handler(void);
-
-void parseIMU(void);
-void pollIMU(void);
-void VCU_MAIN(void);
-
-/* Torque Vectoring Definitions */
-static ExtU_tv rtU_tv; /* External inputs */
-static ExtY_tv rtY_tv; /* External outputs */
-static RT_MODEL_tv rtM_tvv;
-static RT_MODEL_tv *const rtMPtr_tv = &rtM_tvv; /* Real-time model */
-static DW_tv rtDW_tv;                        /* Observable states */
-static RT_MODEL_tv *const rtM_tv = rtMPtr_tv;
-static int16_t tv_timing;
-
-/* Engine Map Definitions */
-static ExtU_em rtU_em; /* External inputs */
-static ExtY_em rtY_em; /* External outputs */
-static RT_MODEL_em rtM_emv;
-static RT_MODEL_em *const rtMPtr_em = &rtM_emv; /* Real-time model */
-static DW_em rtDW_em;                        /* Observable states */
-static RT_MODEL_em *const rtM_em = rtMPtr_em;
-static int16_t em_timing;
-
-/* Moving Median Definition */
-static vec_accumulator vec_mm; /* Vector of Accumulation */
-static int16_t ac_counter = 0; /* Number of data points collected */
-static bool TV_Calibrated = false; /* Flag Indicating if TV is calibrated */
-static int16_t gyro_counter = 0; /* Number of steps that gyro has not been checked */
 
 int main(void)
 {
@@ -206,11 +95,6 @@ int main(void)
     taskCreateBackground(canRxUpdate);
 
     taskCreate(heartBeatLED, 500);
-    taskCreate(heartBeatTask, 100);
-
-    taskCreate(parseIMU, 20);
-    taskCreate(pollIMU, 20);
-    taskCreate(VCU_MAIN, 15);
 
     /* No Way Home */
     schedStart();
@@ -231,66 +115,9 @@ void preflightChecks(void)
         }
         NVIC_EnableIRQ(CAN1_RX0_IRQn);
         break;
-    case 2:
-        /* USART initialization */
-        if (!PHAL_initUSART(&huart_gps, APB1ClockRateHz))
-        {
-            HardFault_Handler();
-        }
-    break;
-    case 3:
-        // GPS Initialization
-        PHAL_writeGPIO(GPS_RESET_GPIO_Port, GPS_RESET_Pin, 1);
-        PHAL_usartRxDma(&huart_gps, (uint16_t *)GPSHandle.raw_message, 100, 1);
-    break;
-    case 5:
-        initFaultLibrary(FAULT_NODE_NAME, &q_tx_can[CAN1_IDX][CAN_MAILBOX_HIGH_PRIO], ID_FAULT_SYNC_TORQUE_VECTOR);
-        break;
-    case 1:
-        /* SPI initialization */
-        if (!PHAL_SPI_init(&spi_config))
-        {
-            HardFault_Handler();
-        }
-        spi_config.data_rate = APB2ClockRateHz / 16;
-
-        PHAL_writeGPIO(SPI_CS_ACEL_GPIO_Port, SPI_CS_ACEL_Pin, 1);
-        PHAL_writeGPIO(SPI_CS_GYRO_GPIO_Port, SPI_CS_GYRO_Pin, 1);
-        PHAL_writeGPIO(SPI_CS_MAG_GPIO_Port, SPI_CS_MAG_Pin, 1);
-    break;
-    case 4:
-        if (!BMI088_init(&bmi_config))
-        {
-            HardFault_Handler();
-        }
-        break;
-    case 250:
-        BMI088_powerOnAccel(&bmi_config);
-        break;
-    case 500:
-        if (!BMI088_initAccel(&bmi_config))
-            HardFault_Handler();
-        break;
-    case 700:
-        /* Pack torque vectoring data into rtM_tv */
-        rtM_tv->dwork = &rtDW_tv;
-
-        /* Initialize Torque Vectoring */
-        tv_initialize(rtM_tv);
-
-        /* Initialize TV IO */
-        tv_IO_initialize(&rtU_tv);
-
-        /* Pack Engine map data into rtM_em */
-        rtM_em->dwork = &rtDW_em;
-
-        /* Initialize Engine Map */
-        em_initialize(rtM_em);
     default:
         if (state > 750)
         {
-            if (!imu_init(&imu_h))
-                HardFault_Handler();
             initCANParse();
             registerPreflightComplete(1);
             state = 750; // prevent wrap around
@@ -340,101 +167,10 @@ void heartBeatLED(void)
     trig = !trig;
 }
 
-void pollIMU(void)
-{
-    imu_periodic(&imu_h);
-}
-
-void parseIMU(void)
-{
-    GPSHandle.messages_received++;
-    BMI088_readGyro(&bmi_config, &gyro_in);
-    BMI088_readAccel(&bmi_config, &accel_in);
-    GPSHandle.acceleration = accel_in;
-    GPSHandle.gyroscope = gyro_in;
-
-    /* Update Gyro OK flag */
-    if (gyro_counter == 150){
-        GPSHandle.gyro_OK = BMI088_gyroOK(&bmi_config);
-        gyro_counter = 0;
-    } else {
-        ++gyro_counter;
-    }
-}
-
-void usart_recieve_complete_callback(usart_init_t *handle)
-{
-   parseVelocity(&GPSHandle);
-}
-
 /* CAN Message Handling */
 void CAN1_RX0_IRQHandler()
 {
     canParseIRQHandler(CAN1);
-}
-
-void VCU_MAIN(void)
-{
-    /* Initialize Throttles */
-    int16_t tvs_k_rl = 0;
-    int16_t tvs_k_rr = 0;
-    int16_t equal_k_rl = 0;
-    int16_t equal_k_rr = 0;
-
-    /* If precharging -> Accumulate acceleration vector */
-    if (/*(can_data.main_hb.car_state == 1) & */(ac_counter <= NUM_ELEM_ACC_CALIBRATION)) {
-        /* Accumulate acceleration vector */
-        vec_mm.ax[ac_counter] = GPSHandle.acceleration.x;
-        vec_mm.ay[ac_counter] = GPSHandle.acceleration.y;
-        vec_mm.az[ac_counter] = GPSHandle.acceleration.z;
-        ++ac_counter;
-
-        /* If length of acceleration vector == ##, compute R */
-        if (ac_counter == NUM_ELEM_ACC_CALIBRATION) {
-            ac_compute_R(vec_mm.ax, vec_mm.ay, vec_mm.az, rtU_tv.R);
-            TV_Calibrated = true;
-        }
-    }
-
-    /* Populate torque vectoring inputs */
-    tv_pp(&rtU_tv, &GPSHandle);
-
-    /* Step torque vectoring */
-    tv_timing = sched.os_ticks;
-    tv_step(rtMPtr_tv, &rtU_tv, &rtY_tv);
-    tv_timing = sched.os_ticks - tv_timing;
-
-    /* Populate engine map inputs */
-    em_pp(&rtU_em, &rtY_tv);
-
-    /* Step engine map */
-    em_timing = sched.os_ticks;
-    em_step(rtMPtr_em, &rtU_em, &rtY_em);
-    em_timing = sched.os_ticks - em_timing;
-
-    /* Set TV faults */
-    setFault(ID_TV_DISABLED_FAULT,!rtY_tv.TVS_STATE);
-    setFault(ID_MM_DISABLED_FAULT,!rtY_em.MM_STATE);
-    setFault(ID_TV_UNCALIBRATED_FAULT,!TV_Calibrated);
-    setFault(ID_NO_GPS_FIX_FAULT,!rtU_tv.F_raw[8]);
-
-    setFault(ID_TV_ENABLED_FAULT,rtY_tv.TVS_STATE);
-    setFault(ID_MM_ENABLED_FAULT,rtY_em.MM_STATE);
-    setFault(ID_TV_CALIBRATED_FAULT,TV_Calibrated);
-    setFault(ID_YES_GPS_FIX_FAULT,rtU_tv.F_raw[8]);
-
-    /* Get motor commands */
-    tvs_k_rl = (int16_t)(rtY_em.kTVS[0]*4095);
-    tvs_k_rr = (int16_t)(rtY_em.kTVS[1]*4095);
-    equal_k_rl = (int16_t)(rtY_em.kEQUAL[0]*4095);
-    equal_k_rr = (int16_t)(rtY_em.kEQUAL[1]*4095);
-
-    /* Send messages */
-    SEND_THROTTLE_VCU(tvs_k_rl,tvs_k_rr);
-    SEND_MAXR((int16_t)(rtY_tv.max_K*4095));
-
-    SEND_SFS_ACC((int16_t)(rtY_tv.sig_filt[15] * 100),(int16_t)(rtY_tv.sig_filt[16] * 100), (int16_t)(rtY_tv.sig_filt[17] * 100));
-    SEND_SFS_ANG_VEL((int16_t)(rtY_tv.sig_filt[7] * 10000),(int16_t)(rtY_tv.sig_filt[8] * 10000), (int16_t)(rtY_tv.sig_filt[9] * 10000));
 }
 
 void torquevector_bl_cmd_CALLBACK(CanParsedData_t *msg_data_a)

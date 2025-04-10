c0400fe527d5c40e5b4585fd17ae11053b4e5a17
Eileen Yoon <eyn@gmx.com>
2025-04-10 13:55:09 -0400

    f4 testing

--- source/f4_testing/f4_testing.h
+++ source/f4_testing/f4_testing.h
@@ -4,8 +4,9 @@
 // To add new tests create a separate file (see led_blink.c) and it to the enum here
 #define TEST_LED_BLINK     0
 #define TEST_FREERTOS_DEMO 1
+#define TEST_IMD_DECODE    2
 
 // Change this define to set the test compiled
-#define F4_TESTING_CHOSEN TEST_FREERTOS_DEMO
+#define F4_TESTING_CHOSEN TEST_IMD_DECODE
 
 #endif // __F4_TESTING__
--- source/f4_testing/imd_decode.c
+++ source/f4_testing/imd_decode.c
@@ -1,11 +1,111 @@
 
+#include "f4_testing.h"
+
+// Guard so cmake doesn't compile all tests
+#if (F4_TESTING_CHOSEN == TEST_IMD_DECODE)
+
 #include "common/common_defs/common_defs.h"
 #include "common/phal_F4_F7/gpio/gpio.h"
 #include "common/phal_F4_F7/rcc/rcc.h"
+#include "common/psched/psched.h"
+#include "common/phal_F4_F7/usart/usart.h"
+#include "common/log/log.h"
 
-#include "main.h"
+#define IMD_HS_PWM_GPIO_Port (GPIOB)
+#define IMD_HS_PWM_Pin (3)
+#define IMD_HS_PWM_AF  (1) // TIM2_CH2
+#define IMD_LS_PWM_GPIO_Port (GPIOA)
+#define IMD_LS_PWM_Pin (15)
+#define IMD_LS_PWM_AF (1) // TIM2_CH1
+#define IMD_STATUS_GPIO_Port (GPIOB)
+#define IMD_STATUS_Pin (4)
+#define IMD_STATUS_AF  (2) // TIM3_CH1
 
-// decode IMD status output PWM signal that changes both frequency and pwm
+void HardFault_Handler();
+void ledblink();
+void imdDecodeInit(void);
+void imdDecodePeriodic(void);
+
+GPIOInitConfig_t gpio_config[] = {
+    GPIO_INIT_OUTPUT(GPIOD, 12, GPIO_OUTPUT_LOW_SPEED), // F407VGT disco LEDs
+    GPIO_INIT_OUTPUT(GPIOD, 13, GPIO_OUTPUT_LOW_SPEED),
+    GPIO_INIT_OUTPUT(GPIOD, 14, GPIO_OUTPUT_LOW_SPEED),
+    GPIO_INIT_OUTPUT(GPIOD, 15, GPIO_OUTPUT_LOW_SPEED),
+
+    GPIO_INIT_USART2TX_PA2,
+    GPIO_INIT_USART2RX_PA3,
+
+    GPIO_INIT_AF(IMD_LS_PWM_GPIO_Port, IMD_LS_PWM_Pin, IMD_LS_PWM_AF, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_PULL_DOWN),
+};
+
+extern uint32_t APB1ClockRateHz;
+extern uint32_t APB2ClockRateHz;
+extern uint32_t AHBClockRateHz;
+extern uint32_t PLLClockRateHz;
+
+volatile uint32_t tick_ms; // Systick 1ms counter
+
+#define TargetCoreClockrateHz 16000000
+ClockRateConfig_t clock_config = {
+    .clock_source               =CLOCK_SOURCE_HSI,
+    .use_pll                    =false,
+    .vco_output_rate_target_hz  =160000000,
+    .system_clock_target_hz     =TargetCoreClockrateHz,
+    .ahb_clock_target_hz        =(TargetCoreClockrateHz / 1),
+    .apb1_clock_target_hz       =(TargetCoreClockrateHz / (1)),
+    .apb2_clock_target_hz       =(TargetCoreClockrateHz / (1)),
+};
+
+dma_init_t usart_tx_dma_config = USART2_TXDMA_CONT_CONFIG(NULL, 1);
+dma_init_t usart_rx_dma_config = USART2_RXDMA_CONT_CONFIG(NULL, 2);
+usart_init_t usart_config = {
+   .baud_rate   = 115200,
+   .word_length = WORD_8,
+   .stop_bits   = SB_ONE,
+   .parity      = PT_NONE,
+   .hw_flow_ctl = HW_DISABLE,
+   .ovsample    = OV_16,
+   .obsample    = OB_DISABLE,
+   .periph      = USART2,
+   .wake_addr = false,
+   .usart_active_num = USART2_ACTIVE_IDX,
+   .tx_dma_cfg = &usart_tx_dma_config,
+   .rx_dma_cfg = &usart_rx_dma_config
+};
+DEBUG_PRINTF_USART_DEFINE(&usart_config) // use LTE uart lmao
+
+int main()
+{
+    if(0 != PHAL_configureClockRates(&clock_config))
+    {
+        HardFault_Handler();
+    }
+    if(!PHAL_initGPIO(gpio_config, sizeof(gpio_config)/sizeof(GPIOInitConfig_t)))
+    {
+        HardFault_Handler();
+    }
+    SysTick_Config(SystemCoreClock / 1000);
+    NVIC_EnableIRQ(SysTick_IRQn);
+
+    if(!PHAL_initUSART(&usart_config, APB1ClockRateHz))
+    {
+        HardFault_Handler();
+    }
+    log_yellow("PER PER PER\n");
+    imdDecodeInit();
+
+    schedInit(APB1ClockRateHz);
+    taskCreate(ledblink, 500);
+    taskCreate(imdDecodePeriodic, 100);
+    schedStart();
+
+    return 0;
+}
+
+void ledblink()
+{
+    PHAL_toggleGPIO(GPIOD, 13);
+}
 
 void imdDecodeInit(void)
 {
@@ -22,7 +122,6 @@ void imdDecodeInit(void)
     TIM2->PSC   =  0;                                // Do not scale the counter clock
     TIM2->CNT   =  0;                                // Reset the counter value
     TIM2->ARR   =  0xFFFFFFFF;                       // Set auto reload to allow for maximum counter value
-
 
     // Disable capture from the counter for channels 1 and 2
     TIM2->CCER &= ~(TIM_CCER_CC1E | TIM_CCER_CC2E);
@@ -87,6 +186,7 @@ void imdDecodePeriodic(void)
     float frequency = (float)APB1ClockRateHz / (period + 1);
     float duty_cycle = ((duty + 1) * 100) / (period);
     //SEND_IMD_STATUS_RAW(period, duty);
+    debug_printf("period: %d duty: %d\n", period, duty);
 }
 
 #if 0
@@ -100,3 +200,17 @@ void TIM2_IRQHandler(void)
   }
 }
 #endif
+
+void HardFault_Handler()
+{
+    while(1)
+    {
+        __asm__("nop");
+    }
+}
+
+#endif // F4_TESTING_CHOSEN == TEST_IMD_DECODE
+
+
+
+// decode IMD status output PWM signal that changes both frequency and pwm

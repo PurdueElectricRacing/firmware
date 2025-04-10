b720e2400a0e9dc881cf62b89be277ecc0a03806
Eileen Yoon <eyn@gmx.com>
2025-04-10 15:13:20 -0400

    tmp

--- source/f4_testing/imd_decode.c
+++ source/f4_testing/imd_decode.c
@@ -4,6 +4,8 @@
 // Guard so cmake doesn't compile all tests
 #if (F4_TESTING_CHOSEN == TEST_IMD_DECODE)
 
+// decode IMD status output PWM signal that changes both frequency and pwm
+
 #include "common/common_defs/common_defs.h"
 #include "common/phal_F4_F7/gpio/gpio.h"
 #include "common/phal_F4_F7/rcc/rcc.h"
@@ -21,6 +23,14 @@
 #define IMD_STATUS_Pin (4)
 #define IMD_STATUS_AF  (2) // TIM3_CH1
 
+#define IMD_LS_PWM1_GPIO_Port (GPIOA)
+#define IMD_LS_PWM1_Pin (0)
+#define IMD_LS_PWM1_AF (1) // PA0 TIM2_CH1_ETR
+
+#define IMD_LS_PWM2_GPIO_Port (GPIOA)
+#define IMD_LS_PWM2_Pin (1)
+#define IMD_LS_PWM2_AF (1) // PA1 TIM2_CH2
+
 void HardFault_Handler();
 void ledblink();
 void imdDecodeInit(void);
@@ -35,7 +45,8 @@ GPIOInitConfig_t gpio_config[] = {
     GPIO_INIT_USART2TX_PA2,
     GPIO_INIT_USART2RX_PA3,
 
-    GPIO_INIT_AF(IMD_LS_PWM_GPIO_Port, IMD_LS_PWM_Pin, IMD_LS_PWM_AF, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_PULL_DOWN),
+    GPIO_INIT_AF(IMD_LS_PWM1_GPIO_Port, IMD_LS_PWM1_Pin, IMD_LS_PWM1_AF, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_PULL_UP),
+    GPIO_INIT_AF(IMD_LS_PWM2_GPIO_Port, IMD_LS_PWM2_Pin, IMD_LS_PWM2_AF, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_PULL_UP),
 };
 
 extern uint32_t APB1ClockRateHz;
@@ -52,8 +63,8 @@ ClockRateConfig_t clock_config = {
     .vco_output_rate_target_hz  =160000000,
     .system_clock_target_hz     =TargetCoreClockrateHz,
     .ahb_clock_target_hz        =(TargetCoreClockrateHz / 1),
-    .apb1_clock_target_hz       =(TargetCoreClockrateHz / (1)),
-    .apb2_clock_target_hz       =(TargetCoreClockrateHz / (1)),
+    .apb1_clock_target_hz       =(TargetCoreClockrateHz / (4)),
+    .apb2_clock_target_hz       =(TargetCoreClockrateHz / (4)),
 };
 
 dma_init_t usart_tx_dma_config = USART2_TXDMA_CONT_CONFIG(NULL, 1);
@@ -74,6 +85,12 @@ usart_init_t usart_config = {
 };
 DEBUG_PRINTF_USART_DEFINE(&usart_config) // use LTE uart lmao
 
+volatile uint16_t period = 0;
+volatile uint16_t duty = 0;
+volatile float frequency = 0;
+volatile float duty_cycle = 0;
+volatile uint64_t overflow_counts = 0;
+
 int main()
 {
     if(0 != PHAL_configureClockRates(&clock_config))
@@ -84,8 +101,8 @@ int main()
     {
         HardFault_Handler();
     }
-    SysTick_Config(SystemCoreClock / 1000);
-    NVIC_EnableIRQ(SysTick_IRQn);
+    //SysTick_Config(SystemCoreClock / 1000);
+    //NVIC_EnableIRQ(SysTick_IRQn);
 
     if(!PHAL_initUSART(&usart_config, APB1ClockRateHz))
     {
@@ -96,7 +113,7 @@ int main()
 
     schedInit(APB1ClockRateHz);
     taskCreate(ledblink, 500);
-    taskCreate(imdDecodePeriodic, 100);
+    //taskCreate(imdDecodePeriodic, 5);
     schedStart();
 
     return 0;
@@ -118,10 +135,10 @@ void imdDecodeInit(void)
         RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
     #endif
 
-    TIM2->CR1   &= ~TIM_CR1_CEN;                     // Disable counter during configuration
-    TIM2->PSC   =  0;                                // Do not scale the counter clock
+    TIM2->CR1   &= ~(TIM_CR1_CEN);                     // Disable counter during configuration
+    TIM2->PSC   = 16 - 1;                                // Do not scale the counter clock
     TIM2->CNT   =  0;                                // Reset the counter value
-    TIM2->ARR   =  0xFFFFFFFF;                       // Set auto reload to allow for maximum counter value
+    TIM2->ARR   = 0xffff - 1;                       // Set auto reload to allow for maximum counter value
 
     // Disable capture from the counter for channels 1 and 2
     TIM2->CCER &= ~(TIM_CCER_CC1E | TIM_CCER_CC2E);
@@ -130,16 +147,19 @@ void imdDecodeInit(void)
     // select channel 1 as the active input
     TIM2->CCMR1 &= ~TIM_CCMR1_CC1S_Msk; // clear bits
     TIM2->CCMR1 |= TIM_CCMR1_CC1S_0;    // 01 - maps ch1 to TI1
+	/*Set CH1 to input capture*/
+	TIM2->CCMR1 = TIM_CCMR1_CC1S;
 
     // setup input capture filtering
     TIM2->CCMR1 &= ~TIM_CCMR1_IC1F_Msk; // clear bits
+    TIM2->CCMR1 |= (0b0011 << 4);
     // select the edge of transition
-    TIM2->CCER &= ~(TIM_CCER_CC2P | TIM_CCER_CC2NP); // 00 - active rising
+    //TIM2->CCER &= ~(TIM_CCER_CC2P | TIM_CCER_CC2NP); // 00 - active rising
+    TIM2->CCER = TIM_CCER_CC1E;
     // set input Pre-Scaler
     TIM2->CCMR1 &= ~TIM_CCMR1_IC1PSC_Msk; // clear bits
 
-
-
+#if 1
     /** configure channel 2 for active falling input **/
     // select channel 2 as the active input
     TIM2->CCMR1 &= ~TIM_CCMR1_CC2S_Msk; // clear bits
@@ -153,6 +173,7 @@ void imdDecodeInit(void)
 
     // set input Pre-Scaler
     TIM2->CCMR1 &= ~TIM_CCMR1_IC2PSC_Msk; // clear bits
+    TIM2->CCMR1 |= (0b0011 << 12);
 
     // select valid trigger input
     TIM2->SMCR &= ~TIM_SMCR_TS_Msk; // clear bits
@@ -161,7 +182,7 @@ void imdDecodeInit(void)
     // configure the slave mode controller in reset mode
     TIM2->SMCR &= ~TIM_SMCR_SMS_Msk; // clear bits
     TIM2->SMCR |= TIM_SMCR_SMS_2; // 100 - rising edge resets the counter
-
+#endif
 
     /** turn stuff on and configure DMA requests **/
     // Enable capture from the counter for channel 1/2
@@ -171,22 +192,50 @@ void imdDecodeInit(void)
 
     // enable counter
     TIM2->CR1 |= TIM_CR1_CEN;
-    #if 0
-    // set interrupt priority
-    NVIC_SetPriority(TIM2_IRQn, 0); // highest priority
-    // enable TIM5 interrupt on NVIC
-    NVIC_EnableIRQ(TIM2_IRQn);
-    #endif
+    //NVIC_SetPriority(TIM2_IRQn, 0); // highest priority
+    //NVIC_EnableIRQ(TIM2_IRQn);
 }
 
 void imdDecodePeriodic(void)
 {
-    uint16_t period = TIM2->CCR1;
-    uint16_t duty = TIM2->CCR2;
-    float frequency = (float)APB1ClockRateHz / (period + 1);
-    float duty_cycle = ((duty + 1) * 100) / (period);
+    period = TIM2->CCR1;
+    duty = TIM2->CCR2;
+    frequency = (float)APB1ClockRateHz / (period + 1);
+    duty_cycle = (float)((duty + 1) * 100) / (period);
     //SEND_IMD_STATUS_RAW(period, duty);
-    debug_printf("period: %d duty: %d\n", period, duty);
+    //debug_printf("period: %d duty: %d\n", period, duty);
+}
+
+void TIM2_IRQHandler(void)
+{
+ /* check for timer overflow */
+ if ((TIM2->SR & TIM_SR_UIF) != 0)
+ {
+  // clear UIF flag to prevent re-entry
+  TIM2->SR &= ~TIM_SR_UIF;
+  overflow_counts++;
+ }
+
+ /* check for rising edge interrupt on channel 1*/
+ if ((TIM2->SR & TIM_SR_CC1IF) != 0)
+ {
+  // read CCR1 to clear CC1IF flag
+  // compute period
+  period = TIM2->CCR1 + ((1 + TIM2->ARR) * overflow_counts);
+  // reset overflows since timer restarts on rising edge
+  overflow_counts = 0;
+ }
+
+ /* check for falling edge interrupt */
+ if ((TIM2->SR & TIM_SR_CC2IF) != 0)
+ {
+  // read CCR2 to clear CC2IF flag
+  /* since the counter resets at each rising edge, the falling edge
+   * is equal to the pulse width */
+  // compute Pulse Width in clocks
+  // pulse_width = count_at_falling + (1 + TIM5->ARR)*overflow_counts;
+  duty = TIM2->CCR2;
+ }
 }
 
 #if 0
@@ -210,7 +259,3 @@ void HardFault_Handler()
 }
 
 #endif // F4_TESTING_CHOSEN == TEST_IMD_DECODE
-
-
-
-// decode IMD status output PWM signal that changes both frequency and pwm

050b4e953da1145c3df01b1df654896c8ac52d12
Irving Wang <wang5952@purdue.edu>
2025-04-14 20:57:12 -0400

    dash changes
    - overload
    - pumps on race
    - igbt

--- source/dashboard/can/can_parse.c
+++ source/dashboard/can/can_parse.c
@@ -203,6 +203,22 @@ void canRxUpdate()
                 can_data.INVB_TEMPS.stale = 0;
                 can_data.INVB_TEMPS.last_rx = sched.os_ticks;
                 break;
+            case ID_INVA_CRIT:
+                can_data.INVA_CRIT.AMK_ActualSpeed = (int16_t) msg_data_a->INVA_CRIT.AMK_ActualSpeed;
+                can_data.INVA_CRIT.AMK_ActualTorque = (int16_t) msg_data_a->INVA_CRIT.AMK_ActualTorque;
+                can_data.INVA_CRIT.AMK_DisplayOverloadInverter = msg_data_a->INVA_CRIT.AMK_DisplayOverloadInverter;
+                can_data.INVA_CRIT.AMK_DisplayOverloadMotor = msg_data_a->INVA_CRIT.AMK_DisplayOverloadMotor;
+                can_data.INVA_CRIT.stale = 0;
+                can_data.INVA_CRIT.last_rx = sched.os_ticks;
+                break;
+            case ID_INVB_CRIT:
+                can_data.INVB_CRIT.AMK_ActualSpeed = (int16_t) msg_data_a->INVB_CRIT.AMK_ActualSpeed;
+                can_data.INVB_CRIT.AMK_ActualTorque = (int16_t) msg_data_a->INVB_CRIT.AMK_ActualTorque;
+                can_data.INVB_CRIT.AMK_DisplayOverloadInverter = msg_data_a->INVB_CRIT.AMK_DisplayOverloadInverter;
+                can_data.INVB_CRIT.AMK_DisplayOverloadMotor = msg_data_a->INVB_CRIT.AMK_DisplayOverloadMotor;
+                can_data.INVB_CRIT.stale = 0;
+                can_data.INVB_CRIT.last_rx = sched.os_ticks;
+                break;
             case ID_FAULT_SYNC_PDU:
                 can_data.fault_sync_pdu.idx = msg_data_a->fault_sync_pdu.idx;
                 can_data.fault_sync_pdu.latched = msg_data_a->fault_sync_pdu.latched;
@@ -296,6 +312,12 @@ void canRxUpdate()
     CHECK_STALE(can_data.INVB_TEMPS.stale,
                 sched.os_ticks, can_data.INVB_TEMPS.last_rx,
                 UP_INVB_TEMPS);
+    CHECK_STALE(can_data.INVA_CRIT.stale,
+                sched.os_ticks, can_data.INVA_CRIT.last_rx,
+                UP_INVA_CRIT);
+    CHECK_STALE(can_data.INVB_CRIT.stale,
+                sched.os_ticks, can_data.INVB_CRIT.last_rx,
+                UP_INVB_CRIT);
     /* END AUTO STALE CHECKS */
 }
 
@@ -339,18 +361,21 @@ bool initCANFilter()
     CAN1->sFilterRegister[7].FR2 = (ID_INVA_TEMPS << 21);
     CAN1->FA1R |= (1 << 8);    // configure bank 8
     CAN1->sFilterRegister[8].FR1 = (ID_INVB_TEMPS << 21);
-    CAN1->sFilterRegister[8].FR2 = (ID_FAULT_SYNC_PDU << 3) | 4;
+    CAN1->sFilterRegister[8].FR2 = (ID_INVA_CRIT << 21);
     CAN1->FA1R |= (1 << 9);    // configure bank 9
-    CAN1->sFilterRegister[9].FR1 = (ID_FAULT_SYNC_MAIN_MODULE << 3) | 4;
-    CAN1->sFilterRegister[9].FR2 = (ID_FAULT_SYNC_A_BOX << 3) | 4;
+    CAN1->sFilterRegister[9].FR1 = (ID_INVB_CRIT << 21);
+    CAN1->sFilterRegister[9].FR2 = (ID_FAULT_SYNC_PDU << 3) | 4;
     CAN1->FA1R |= (1 << 10);    // configure bank 10
-    CAN1->sFilterRegister[10].FR1 = (ID_FAULT_SYNC_TORQUE_VECTOR << 3) | 4;
-    CAN1->sFilterRegister[10].FR2 = (ID_FAULT_SYNC_TEST_NODE << 3) | 4;
+    CAN1->sFilterRegister[10].FR1 = (ID_FAULT_SYNC_MAIN_MODULE << 3) | 4;
+    CAN1->sFilterRegister[10].FR2 = (ID_FAULT_SYNC_A_BOX << 3) | 4;
     CAN1->FA1R |= (1 << 11);    // configure bank 11
-    CAN1->sFilterRegister[11].FR1 = (ID_SET_FAULT << 3) | 4;
-    CAN1->sFilterRegister[11].FR2 = (ID_RETURN_FAULT_CONTROL << 3) | 4;
+    CAN1->sFilterRegister[11].FR1 = (ID_FAULT_SYNC_TORQUE_VECTOR << 3) | 4;
+    CAN1->sFilterRegister[11].FR2 = (ID_FAULT_SYNC_TEST_NODE << 3) | 4;
     CAN1->FA1R |= (1 << 12);    // configure bank 12
-    CAN1->sFilterRegister[12].FR1 = (ID_DAQ_COMMAND_DASHBOARD_VCAN << 3) | 4;
+    CAN1->sFilterRegister[12].FR1 = (ID_SET_FAULT << 3) | 4;
+    CAN1->sFilterRegister[12].FR2 = (ID_RETURN_FAULT_CONTROL << 3) | 4;
+    CAN1->FA1R |= (1 << 13);    // configure bank 13
+    CAN1->sFilterRegister[13].FR1 = (ID_DAQ_COMMAND_DASHBOARD_VCAN << 3) | 4;
     /* END AUTO FILTER */
 
     CAN1->FMR  &= ~CAN_FMR_FINIT;             // Enable Filters (exit filter init mode)

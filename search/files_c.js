f076561b2dcad5930663e47b865e7a664e623b52
eiln <eiln@users.noreply.github.com>
2025-07-06 00:16:29 +0000

    deploy: 7779220cf8bf1acc6799217abcc3b7c82e8b3628

--- search/files_c.js
+++ search/files_c.js
@@ -2,8 +2,9 @@ var searchData=
 [
   ['pedals_2ec_0',['pedals.c',['../pedals_8c.html',1,'']]],
   ['pedals_2eh_1',['pedals.h',['../pedals_8h.html',1,'']]],
-  ['plettenberg_2ec_2',['plettenberg.c',['../plettenberg_8c.html',1,'']]],
-  ['plettenberg_2eh_3',['plettenberg.h',['../plettenberg_8h.html',1,'']]],
-  ['psched_2ec_4',['psched.c',['../psched_8c.html',1,'']]],
-  ['psched_2eh_5',['psched.h',['../psched_8h.html',1,'']]]
+  ['phal_5ff4_5ff7_2eh_2',['phal_F4_F7.h',['../phal__F4__F7_8h.html',1,'']]],
+  ['plettenberg_2ec_3',['plettenberg.c',['../plettenberg_8c.html',1,'']]],
+  ['plettenberg_2eh_4',['plettenberg.h',['../plettenberg_8h.html',1,'']]],
+  ['psched_2ec_5',['psched.c',['../psched_8c.html',1,'']]],
+  ['psched_2eh_6',['psched.h',['../psched_8h.html',1,'']]]
 ];

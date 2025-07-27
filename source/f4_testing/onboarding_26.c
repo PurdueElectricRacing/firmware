acffa43e73f3998fcdb3dab2f4d4b8ce6b4623bf
Eileen Yoon <eyn@gmx.com>
2025-07-27 12:32:23 -0700

    onboarding: Add onboarding 26 template (#208)
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

---
+++ source/f4_testing/onboarding_26.c
@@ -0,0 +1,81 @@
+/**
+ * @file onboarding_26.c
+ * @author Eileen Yoon (eyn@purdue.edu)
+ * @brief  Onboarding 26 starter file
+ * @version 0.1
+ * @date 2026-07-27
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+#include "f4_testing.h"
+
+#if (F4_TESTING_CHOSEN == TEST_ONBOARDING_26)
+
+#include "common/freertos/freertos.h"
+#include "common/phal/gpio.h"
+#include "common/phal/rcc.h"
+
+GPIOInitConfig_t gpio_config[] = {
+    GPIO_INIT_OUTPUT(GPIOD, 12, GPIO_OUTPUT_LOW_SPEED),
+    // TODO: Add GPIO LEDs here...
+};
+
+extern uint32_t APB1ClockRateHz;
+extern uint32_t APB2ClockRateHz;
+extern uint32_t AHBClockRateHz;
+extern uint32_t PLLClockRateHz;
+
+#define TargetCoreClockrateHz 16000000
+ClockRateConfig_t clock_config = {
+    .clock_source = CLOCK_SOURCE_HSI,
+    .use_pll = false,
+    .vco_output_rate_target_hz = 160000000,
+    .system_clock_target_hz = TargetCoreClockrateHz,
+    .ahb_clock_target_hz = (TargetCoreClockrateHz / 1),
+    .apb1_clock_target_hz = (TargetCoreClockrateHz / (1)),
+    .apb2_clock_target_hz = (TargetCoreClockrateHz / (1)),
+};
+
+void HardFault_Handler();
+void ledblink1();
+// TODO add more function definitions here
+
+defineThreadStack(ledblink1, 250, osPriorityNormal, 64);
+
+// TODO add thread definitions here
+
+int main() {
+    osKernelInitialize();
+
+    // Initialize hardware
+    if (0 != PHAL_configureClockRates(&clock_config)) {
+        HardFault_Handler();
+    }
+    if (!PHAL_initGPIO(gpio_config, sizeof(gpio_config) / sizeof(GPIOInitConfig_t))) {
+        HardFault_Handler();
+    }
+
+    // Create threads
+    createThread(ledblink1);
+    // TODO: Create threads here
+
+    osKernelStart(); // Go!
+
+    return 0;
+}
+
+void ledblink1() {
+    PHAL_toggleGPIO(GPIOD, 12);
+}
+
+// TODO: add more function definitions here
+
+void HardFault_Handler() {
+    while (1) {
+        __asm__("nop");
+    }
+}
+
+#endif // F4_TESTING_CHOSEN == TEST_ONBOARDING_26

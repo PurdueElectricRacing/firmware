3542b0acac679b4e0500b355cb4b75b5ead6162c
Eileen Yoon <eyn@gmx.com>
2025-07-27 12:22:41 -0700

    onboarding: Add onboarding 26 template
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- source/f4_testing/f4_testing.h
+++ source/f4_testing/f4_testing.h
@@ -4,8 +4,9 @@
 // To add new tests create a separate file (see led_blink.c) and it to the enum here
 #define TEST_LED_BLINK     0
 #define TEST_FREERTOS_DEMO 1
+#define TEST_ONBOARDING_26 2
 
 // Change this define to set the test compiled
-#define F4_TESTING_CHOSEN TEST_FREERTOS_DEMO
+#define F4_TESTING_CHOSEN TEST_ONBOARDING_26
 
 #endif // __F4_TESTING__

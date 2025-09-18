f0c31ef777a03b212cae53f33bd335d599f6086b
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-09-18 13:47:01 -0400

    testing

--- source/g4_testing/g4_testing.h
+++ source/g4_testing/g4_testing.h
@@ -4,8 +4,9 @@
 // To add new tests create a separate file (see led_blink.c) and it to the enum here
 #define TEST_BLINKY 0
 #define TEST_FDCAN  1
+#define TEST_USART  2
 
 // Change this define to set the test compiled
-#define G4_TESTING_CHOSEN TEST_FDCAN
+#define G4_TESTING_CHOSEN TEST_USART
 
 #endif // __G4_TESTING__

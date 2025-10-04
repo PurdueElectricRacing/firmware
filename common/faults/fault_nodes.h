5fdcdb540a3741f55d2d2a53145129400cd3434d
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-10-04 11:03:43 -0400

    cleanup

--- common/faults/fault_nodes.h
+++ common/faults/fault_nodes.h
@@ -2,12 +2,12 @@
 #define _FAULT_NODES_H_
 
 //BEGIN AUTO NODE DEFS
-#define NODE_PDU           0
-#define NODE_MAIN_MODULE   1
-#define NODE_DASHBOARD     2
-#define NODE_A_BOX         3
+#define NODE_PDU 0
+#define NODE_MAIN_MODULE 1
+#define NODE_DASHBOARD 2
+#define NODE_A_BOX 3
 #define NODE_TORQUE_VECTOR 4
-#define NODE_TEST          5
+#define NODE_TEST 5
 //END AUTO NODE DEFS
 
 #endif

82ce5c5dbcd3b16a125ca7f75c1e51a4d0c6e712
Ronak Jain <ronak@ronakpjain.com>
2025-10-16 18:11:15 -0400

    compile cmsis-dsp

--- cmake/FindDSP.cmake
+++ cmake/FindDSP.cmake
@@ -24,23 +24,42 @@ function(make_cmsis_dsp_library LIB_NAME DIRNAME LINK_NAME)
         message(WARNING "No CMSIS-DSP source files found in ${LIB_PATH}/Source")
     endif()
 
-    # Link with the device CMSIS core library
+    # Link with the device CMSIS core library (CMSIS_F407, CMSIS_F732, etc.)
     target_link_libraries(${LIB_NAME} ${LINK_NAME})
 
-    # Determine the ARM core type from the directory name
-    if(${DIRNAME} MATCHES "L4" OR ${DIRNAME} MATCHES "G4" OR ${DIRNAME} MATCHES "F4")
-        set(ARM_CORE_DEF "ARM_MATH_CM4")
-    elseif(${DIRNAME} MATCHES "F7")
+    if(${DIRNAME} MATCHES "STM32CubeF7")
+        set(_ARCH_FLAGS -mthumb -mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard)
         set(ARM_CORE_DEF "ARM_MATH_CM7")
+        target_compile_definitions(${LIB_NAME} PRIVATE STM32F732xx STM32F7xx)
+
+    elseif(${DIRNAME} MATCHES "STM32CubeF4")
+        set(_ARCH_FLAGS -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
+        set(ARM_CORE_DEF "ARM_MATH_CM4")
+        target_compile_definitions(${LIB_NAME} PRIVATE STM32F407xx STM32F4xx)
+
+    elseif(${DIRNAME} MATCHES "STM32CubeG4")
+        set(_ARCH_FLAGS -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
+        set(ARM_CORE_DEF "ARM_MATH_CM4")
+        target_compile_definitions(${LIB_NAME} PRIVATE STM32G474xx STM32G4xx)
+
+    elseif(${DIRNAME} MATCHES "STM32CubeL4")
+        set(_ARCH_FLAGS -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
+        set(ARM_CORE_DEF "ARM_MATH_CM4")
+        target_compile_definitions(${LIB_NAME} PRIVATE STM32L496xx STM32L4xx)
+
     else()
-        set(ARM_CORE_DEF "ARM_MATH_CM4") # default fallback
+        set(_ARCH_FLAGS -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard)
+        set(ARM_CORE_DEF "ARM_MATH_CM4")
         message(WARNING "Unknown STM32 family for ${DIRNAME}, defaulting to ARM_MATH_CM4")
     endif()
 
+    # Apply flags
+    target_compile_options(${LIB_NAME} PRIVATE ${_ARCH_FLAGS})
+    target_link_options(${LIB_NAME} PRIVATE ${_ARCH_FLAGS})
     target_compile_definitions(${LIB_NAME} PUBLIC ${ARM_CORE_DEF})
 
     # Disable analyzer warnings for external code
     target_compile_options(${LIB_NAME} PRIVATE -fno-analyzer)
 
-    message(STATUS "Configured CMSIS-DSP library ${LIB_NAME} (${ARM_CORE_DEF})")
+    message(STATUS "Configured CMSIS-DSP library ${LIB_NAME} (${ARM_CORE_DEF}) with flags: ${_ARCH_FLAGS}")
 endfunction()

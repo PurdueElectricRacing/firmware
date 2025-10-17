aad07b6b7bf84cad32e1e13dd069276a7065f15e
Pris1320 <prisha.k.mohan@gmail.com>
2025-10-16 20:34:00 -0400

    Add sendVersion task to main function
    Added a new task to send version information with the current git hash.

--- source/pdu/main.c
+++ source/pdu/main.c
@@ -208,6 +208,8 @@ int main() {
     taskCreate(checkSwitchFaults, 100);
     taskCreate(send_flowrates, 200);
     schedStart();
+    // code added for send version task
+     taskCreate(sendVersion, 5000);
     return 0;
 }
 
@@ -260,6 +262,12 @@ void preflightChecks(void) {
     }
 }
 
+// code added for send version
+void sendVersion() {
+    char git_hash[8] = GIT_HASH;
+    uint64_t git_hash_num = EIGHT_CHAR_TO_U64_LE(git_hash);
+    SEND_DASH_VERSION(git_hash_num);
+}
 void preflightAnimation(void) {
     static uint32_t time;
     static int led_number;

30e0f2285e8ebed25c60265fe17703264928fa33
sethbaird01 <sethbaird01@users.noreply.github.com>
2026-01-21 00:28:32 +0000

    deploy: fcb57f1f0d7c7eaeeb67ede00cb37a6e9ef81ae2

--- search/files_c.js
+++ search/files_c.js
@@ -4,5 +4,8 @@ var searchData=
   ['pedals_2eh_1',['pedals.h',['../pedals_8h.html',1,'']]],
   ['phal_5ff4_5ff7_2eh_2',['phal_F4_F7.h',['../phal__F4__F7_8h.html',1,'']]],
   ['psched_2ec_3',['psched.c',['../psched_8c.html',1,'']]],
-  ['psched_2eh_4',['psched.h',['../psched_8h.html',1,'']]]
+  ['psched_2eh_4',['psched.h',['../psched_8h.html',1,'']]],
+  ['pwm_2ec_5',['pwm.c',['../pwm_8c.html',1,'']]],
+  ['pwm_2eh_6',['pwm.h',['../pwm_8h.html',1,'']]],
+  ['pwm_5ftesting_2ec_7',['pwm_testing.c',['../pwm__testing_8c.html',1,'']]]
 ];

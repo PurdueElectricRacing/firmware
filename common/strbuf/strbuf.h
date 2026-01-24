93562a4c711faf6ef8cc66caf3869ea514d72be0
Irving Wang <wang5952@purdue.edu>
2026-01-24 17:17:35 -0500

    add docs

--- common/strbuf/strbuf.h
+++ common/strbuf/strbuf.h
@@ -12,14 +12,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define allocate_strbuf(NAME, MAX_SIZE) \
-    uint8_t NAME##_data[(MAX_SIZE)]; \
-    strbuf_t NAME = { \
-        .data    = NAME##_data, \
-        .length  = 0, \
-        .max_len = (MAX_SIZE), \
-    }
-
 typedef struct {
     uint8_t *data;
     size_t length;
@@ -28,5 +20,13 @@ typedef struct {
 
 void strbuf_clear(strbuf_t *sb);
 size_t strbuf_append(strbuf_t *sb, const void *data, size_t length);
+
+#define allocate_strbuf(NAME, MAX_SIZE) \
+    uint8_t NAME##_data[(MAX_SIZE)]; \
+    strbuf_t NAME = { \
+        .data    = NAME##_data, \
+        .length  = 0, \
+        .max_len = (MAX_SIZE), \
+    }
 
 #endif // STRBUF_H

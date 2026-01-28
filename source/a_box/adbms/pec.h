114d489ef96114649d4d404a3b404e61cdf01298
LelsersLasers <millankumar@gmail.com>
2026-01-27 21:50:51 -0500

    making const b/c they don't modify

--- source/a_box/adbms/pec.h
+++ source/a_box/adbms/pec.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint16_t adbms_pec_get_pec15(uint8_t len, uint8_t *data);
-uint16_t adbms_pec_get_pec10(bool bIsRxCmd, uint8_t nLength, uint8_t *pDataBuf);
+uint16_t adbms_pec_get_pec15(uint8_t len, const uint8_t *data);
+uint16_t adbms_pec_get_pec10(bool bIsRxCmd, uint8_t nLength, const uint8_t *pDataBuf);
 
 #endif // _ADBMS_PEC_H_

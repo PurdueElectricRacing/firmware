e31a1e5172a6362ad64c5d5e9933aab293b99bee
LelsersLasers <millankumar@gmail.com>
2026-01-27 19:36:52 -0500

    Fixing types/names + state enum

--- common/adbms/bms.h
+++ common/adbms/bms.h
@@ -17,23 +17,31 @@ constexpr size_t ADBMS_MODULE_THERM_COUNT = 10;
 constexpr size_t ADBMS_SPI_TX_BUFFER_SIZE = ADBMS6380_COMMAND_PKT_SIZE + (ADBMS_MODULE_CELL_COUNT * ADBMS6380_SINGLE_DATA_PKT_SIZE);
 constexpr size_t ADBMS_SPI_RX_BUFFER_SIZE = ADBMS_SPI_TX_BUFFER_SIZE; //! Fix for READALL
 
+typedef enum {
+	ADBMS_STATE_IDLE = 0,
+	ADBMS_STATE_CONNECTING,
+	ADBMS_STATE_CONNECTED,
+	ADBMS_STATE_CHARING,
+} ADBMS_state_t;
 
 
 typedef struct {
-	float cell_voltages[MODULE_CELL_COUNT]; // in volts
+	float cell_voltages[ADBMS_MODULE_CELL_COUNT]; // in volts
 	float min_voltage; // in volts
 	float max_voltage; // in volts
 	float avg_voltage; // in volts
 	float sum_voltage; // in volts
-	float thermistors[MODULE_THERM_COUNT]; // in degrees Celsius
+	float thermistors[ADBMS_MODULE_THERM_COUNT]; // in degrees Celsius
 	//! TODO: do we also want min/max/avg for thermistors?
-	bool is_discharging[MODULE_CELL_COUNT]; // if a cell is being discharged for balancing
+	bool is_discharging[ADBMS_MODULE_CELL_COUNT]; // if a cell is being discharged for balancing
 
 	bool error_1; //! Fill in actual errors
-} ADBMS_Module;
+} ADBMS_module_t;
 
 typedef struct {
-	ADBMS_Module modules[ADBMS_MODULE_COUNT];
+	ADBMS_state_t state;
+
+	ADBMS_module_t modules[ADBMS_MODULE_COUNT];
 
 	float min_voltage; // in volts
 	float max_voltage; // in volts
@@ -42,10 +50,10 @@ typedef struct {
 
 	bool is_discharge_enabled; // If the BMS is allowed to discharge cells for balancing
 
-	SPI_InitConfig_t *spi;
+	SPI_InitConfig_t *spi; // Note: must not use auto CS pin, bms driver controls CS manually
 	uint8_t tx_buffer[ADBMS_SPI_TX_BUFFER_SIZE]; //! Change to new buffer type when #247 is done
 	uint8_t rx_buffer[ADBMS_SPI_RX_BUFFER_SIZE];
-} ADBMS_BMS;
+} ADBMS_bms_t;
 
 
 

972b3b901f284fa72e516d2488ffbab67aae670e
LelsersLasers <millankumar@gmail.com>
2026-02-03 20:59:25 -0500

    8 modules

--- source/a_box/adbms/adbms.h
+++ source/a_box/adbms/adbms.h
@@ -10,7 +10,7 @@
 #include "adbms6380.h"
 
 
-#define ADBMS_MODULE_COUNT       (6)
+#define ADBMS_MODULE_COUNT       (8)
 
 // Max SPI TX is a command + all the data packets for all the modules
 #define ADBMS_SPI_TX_BUFFER_SIZE (ADBMS6380_COMMAND_PKT_SIZE + (ADBMS6380_CELL_COUNT * ADBMS6380_SINGLE_DATA_PKT_SIZE))

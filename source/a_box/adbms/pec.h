196c17ba126f5205a5208b26b1517880c131ed8e
LelsersLasers <millankumar@gmail.com>
2026-02-03 21:49:14 -0500

    header comments on pec and commands files

--- source/a_box/adbms/pec.h
+++ source/a_box/adbms/pec.h
@@ -1,3 +1,10 @@
+/**
+ * @file pec.h
+ * @brief Calculation of PEC (CRC15, CRC10) for ADBMS communication.
+ *
+ * @author Millan Kumar (kumar798@purdue.edu), Irving Wang (irvingw@purdue.edu)
+ */
+
 #ifndef _ADBMS_PEC_H_
 #define _ADBMS_PEC_H_
 

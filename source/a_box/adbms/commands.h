25ac17c51be1dd5522a1357bab0fa73bebe9854c
LelsersLasers <millankumar@gmail.com>
2026-02-05 18:16:05 -0500

    fmt

--- source/a_box/adbms/commands.h
+++ source/a_box/adbms/commands.h
@@ -10,13 +10,13 @@
 
 #include <stdint.h>
 
-/* configuration registers commands */
+// Register configuration
 extern const uint8_t WRCFGA[2];
 extern const uint8_t WRCFGB[2];
 extern const uint8_t RDCFGA[2];
 extern const uint8_t RDCFGB[2];
 
-/* Read cell voltage result registers commands */
+// Read cell voltage result registers
 extern const uint8_t RDCVA[2];
 extern const uint8_t RDCVB[2];
 extern const uint8_t RDCVC[2];
@@ -25,7 +25,7 @@ extern const uint8_t RDCVE[2];
 extern const uint8_t RDCVF[2];
 extern const uint8_t RDCVALL[2];
 
-/* Read average cell voltage result registers commands commands */
+// Read average cell voltage result registers
 extern const uint8_t RDACA[2];
 extern const uint8_t RDACB[2];
 extern const uint8_t RDACC[2];
@@ -34,7 +34,7 @@ extern const uint8_t RDACE[2];
 extern const uint8_t RDACF[2];
 extern const uint8_t RDACALL[2];
 
-/* Read s voltage result registers commands */
+// Read S voltage result registers
 extern const uint8_t RDSVA[2];
 extern const uint8_t RDSVB[2];
 extern const uint8_t RDSVC[2];
@@ -43,14 +43,14 @@ extern const uint8_t RDSVE[2];
 extern const uint8_t RDSVF[2];
 extern const uint8_t RDSALL[2];
 
-/* Read c and s results */
+// Read C and S results
 extern const uint8_t RDCSALL[2];
 extern const uint8_t RDACSALL[2];
 
-/* Read all AUX and all Status Registers */
+// Read all AUX and all status registers
 extern const uint8_t RDASALL[2];
 
-/* Read filtered cell voltage result registers*/
+// Read filtered cell voltage result registers
 extern const uint8_t RDFCA[2];
 extern const uint8_t RDFCB[2];
 extern const uint8_t RDFCC[2];
@@ -59,34 +59,34 @@ extern const uint8_t RDFCE[2];
 extern const uint8_t RDFCF[2];
 extern const uint8_t RDFCALL[2];
 
-/* Read aux results */
+// Read aux results
 extern const uint8_t RDAUXA[2];
 extern const uint8_t RDAUXB[2];
 extern const uint8_t RDAUXC[2];
 extern const uint8_t RDAUXD[2];
 
-/* Read redundant aux results */
+// Read redundant aux results
 extern const uint8_t RDRAXA[2];
 extern const uint8_t RDRAXB[2];
 extern const uint8_t RDRAXC[2];
 extern const uint8_t RDRAXD[2];
 
-/* Read status registers */
+// Read status registers
 extern const uint8_t RDSTATA[2];
 extern const uint8_t RDSTATB[2];
 extern const uint8_t RDSTATC[2];
-extern const uint8_t RDSTATCERR[2];              /* ERR */
+extern const uint8_t RDSTATCERR[2];
 extern const uint8_t RDSTATD[2];
 extern const uint8_t RDSTATE[2];
 
-/* Pwm registers commands */
+// PWM registers commands
 extern const uint8_t WRPWMA[2];
 extern const uint8_t RDPWMA[2];
 
 extern const uint8_t WRPWMB[2];
 extern const uint8_t RDPWMB[2];
 
-/* Clear commands */
+// Clear commands
 extern const uint8_t CLRCELL[2];
 extern const uint8_t CLRAUX [2];
 extern const uint8_t CLRSPIN[2];
@@ -94,7 +94,7 @@ extern const uint8_t CLRFLAG[2];
 extern const uint8_t CLRFC[2];
 extern const uint8_t CLOVUV[2];
 
-/* Poll adc command */
+// Poll ADC commands
 extern const uint8_t PLADC[2];
 extern const uint8_t PLAUT[2];
 extern const uint8_t PLCADC[2];
@@ -102,15 +102,15 @@ extern const uint8_t PLSADC[2];
 extern const uint8_t PLAUX1[2];
 extern const uint8_t PLAUX2[2];
 
-/* Diagn command */
+// Diagn command
 extern const uint8_t DIAGN[2];
 
-/* GPIOs Comm commands */
+// GPIOs commands
 extern const uint8_t WRCOMM[2];
 extern const uint8_t RDCOMM[2];
 extern const uint8_t STCOMM[13];
 
-/* Mute and Unmute commands */
+// Mute and Unmute commands
 extern const uint8_t MUTE[2];
 extern const uint8_t UNMUTE[2];
 
@@ -119,7 +119,7 @@ extern const uint8_t SNAP[2];
 extern const uint8_t UNSNAP[2];
 extern const uint8_t SRST[2];
 
-/* Read SID command */
+// Read SID command
 extern const uint8_t RDSID[2];
 
 #endif // _ADBMS_COMMAND_H_

4f86103aea93ec91006c0c63949ba7f4663304ef
LelsersLasers <millankumar@gmail.com>
2026-02-05 18:28:51 -0500

    adding new bms code

---
+++ source/bms_test/adbms/adbms.c
@@ -0,0 +1,279 @@
+/**
+ * @file adbms.c
+ * @brief Primary logic and interface for ADBMS battery management system driver.
+ *
+ * @author Millan Kumar (kumar798@purdue.edu)
+ */
+
+#include "adbms.h"
+
+#include <string.h>
+
+#include "common/phal/spi.h"
+
+#include "adbms6380.h"
+#include "commands.h"
+
+
+void adbms_init(ADBMS_bms_t* bms, SPI_InitConfig_t* spi, uint8_t* tx_buf) {
+	bms->spi = spi;
+	bms->state = ADBMS_STATE_IDLE;
+
+	bms->is_discharge_enabled = false;
+	for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+		for (size_t j = 0; j < ADBMS6380_CELL_COUNT; j++) {
+			bms->modules[i].is_discharging[j] = false;
+		}
+	}
+
+	bms->tx_strbuf = (strbuf_t) {
+		.data = tx_buf,
+		.length = 0,
+		.max_len = ADBMS_SPI_TX_BUFFER_SIZE,
+	};
+}
+
+bool adbms_write_rega(ADBMS_bms_t* bms) {
+	strbuf_clear(&bms->tx_strbuf);
+	adbms6380_prepare_command(&bms->tx_strbuf, WRCFGA);
+	// i must be signed
+	for (int i = ADBMS_MODULE_COUNT - 1; i >= 0; i--) {
+		adbms6380_calculate_cfg_rega(bms->modules[i].rega, ADBMS_REFON, ADBMS_CTH);
+		adbms6380_prepare_data_packet(&bms->tx_strbuf, bms->modules[i].rega);
+	}
+
+	if (!PHAL_SPI_transfer_noDMA(bms->spi, bms->tx_strbuf.data, bms->tx_strbuf.length, 0, NULL)) {
+		bms->err_spi = true;
+		return false;
+	}
+	return true;
+}
+
+bool adbms_write_regb(ADBMS_bms_t* bms) {
+	strbuf_clear(&bms->tx_strbuf);
+	adbms6380_prepare_command(&bms->tx_strbuf, WRCFGB);
+	// i must be signed
+	for (int i = ADBMS_MODULE_COUNT - 1; i >= 0; i--) {
+		adbms6380_calculate_cfg_regb(bms->modules[i].regb, ADBMS_OV_THRESHOLD, ADBMS_UV_THRESHOLD, bms->modules[i].is_discharging);
+		adbms6380_prepare_data_packet(&bms->tx_strbuf, bms->modules[i].regb);
+	}
+
+	if (!PHAL_SPI_transfer_noDMA(bms->spi, bms->tx_strbuf.data, bms->tx_strbuf.length, 0, NULL)) {
+		bms->err_spi = true;
+		return false;
+	}
+	return true;
+}
+
+bool adbms_read_and_check_rega(ADBMS_bms_t* bms) {
+	strbuf_clear(&bms->tx_strbuf);
+	adbms6380_prepare_command(&bms->tx_strbuf, RDCFGA);
+	if (!adbms6380_read_data(bms->spi, ADBMS_MODULE_COUNT, bms->tx_strbuf.data, bms->rx_buf)) {
+		bms->err_spi = true;
+		return false;
+	}
+	bms->err_rega_mismatch = false;
+	for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+		uint8_t* module_data = &bms->rx_buf[i * ADBMS6380_SINGLE_DATA_PKT_SIZE];
+		if (memcmp(&module_data[0], bms->modules[i].rega, ADBMS6380_SINGLE_DATA_RAW_SIZE) != 0) {
+			bms->err_rega_mismatch = true;
+			bms->modules[i].err_rega_mismatch = true;
+		} else {
+			bms->modules[i].err_rega_mismatch = false;
+		}
+	}
+	return !bms->err_rega_mismatch;
+}
+
+bool adbms_read_and_check_regb(ADBMS_bms_t* bms) {
+	strbuf_clear(&bms->tx_strbuf);
+	adbms6380_prepare_command(&bms->tx_strbuf, RDCFGB);
+	if (!adbms6380_read_data(bms->spi, ADBMS_MODULE_COUNT, bms->tx_strbuf.data, bms->rx_buf)) {
+		bms->err_spi = true;
+		return false;
+	}
+	bms->err_regb_mismatch = false;
+	for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+		uint8_t* module_data = &bms->rx_buf[i * ADBMS6380_SINGLE_DATA_PKT_SIZE];
+		if (memcmp(&module_data[0], bms->modules[i].regb, ADBMS6380_SINGLE_DATA_RAW_SIZE) != 0) {
+			bms->err_regb_mismatch = true;
+			bms->modules[i].err_regb_mismatch = true;
+		} else {
+			bms->modules[i].err_regb_mismatch = false;
+		}
+	}
+	return !bms->err_regb_mismatch;
+}
+
+void adbms_connect(ADBMS_bms_t* bms) {
+	adbms6380_wake(bms->spi, ADBMS_MODULE_COUNT);
+
+	if (!adbms_write_rega(bms)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_connect = true;
+		return;
+	}
+	if (!adbms_write_regb(bms)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_connect = true;
+		return;
+	}
+	if (!adbms_read_and_check_rega(bms)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_connect = true;
+		return;
+	}
+	if (!adbms_read_and_check_regb(bms)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_connect = true;
+		return;
+	}
+
+	// Start ADCV
+	strbuf_clear(&bms->tx_strbuf);
+	uint8_t adcv_cmd[2];
+	adbms6380_adcv(adcv_cmd, ADBMS_RD, ADBMS_CONT, ADBMS_DCP, ADBMS_RSTF, ADBMS_OW);
+	adbms6380_prepare_command(&bms->tx_strbuf, adcv_cmd);
+	if (!PHAL_SPI_transfer_noDMA(bms->spi, bms->tx_strbuf.data, bms->tx_strbuf.length, 0, NULL)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_spi = true;
+		bms->err_connect = true;
+		return;
+	}
+
+	// Start ADSV
+	strbuf_clear(&bms->tx_strbuf);
+	uint8_t adsv_cmd[2];
+	adbms6380_adsv(adsv_cmd, ADBMS_CONT, ADBMS_DCP, ADBMS_OW);
+	adbms6380_prepare_command(&bms->tx_strbuf, adsv_cmd);
+	if (!PHAL_SPI_transfer_noDMA(bms->spi, bms->tx_strbuf.data, bms->tx_strbuf.length, 0, NULL)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_spi = true;
+		bms->err_connect = true;
+		return;
+	}
+
+	bms->err_connect = false;
+	bms->state = ADBMS_STATE_CONNECTED;
+}
+
+void adbms_calculate_balance_cells(ADBMS_bms_t* bms, float min_voltage, float min_delta) {
+	if (!bms->is_discharge_enabled) {
+		// Disable all discharging
+		for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+			for (size_t j = 0; j < ADBMS6380_CELL_COUNT; j++) {
+				bms->modules[i].is_discharging[j] = false;
+			}
+		}
+		return;
+	}
+	if (bms->min_voltage < min_voltage) {
+		return;
+	}
+
+	float balance_threshold = bms->min_voltage + min_delta;
+	for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+		for (size_t j = 0; j < ADBMS6380_CELL_COUNT; j++) {
+			if (bms->modules[i].cell_voltages[j] > balance_threshold) {
+				bms->modules[i].is_discharging[j] = true;
+			} else {
+				bms->modules[i].is_discharging[j] = false;
+			}
+		}
+	}
+}
+
+void adbms_balance_and_update_regb(ADBMS_bms_t* bms, float min_voltage, float min_delta) {
+	adbms_calculate_balance_cells(bms, min_voltage, min_delta);
+
+	if (!adbms_write_regb(bms)) {
+		bms->state = ADBMS_STATE_IDLE;
+		return;
+	}
+	if (!adbms_read_and_check_regb(bms)) {
+		bms->state = ADBMS_STATE_IDLE;
+		return;
+	}
+}
+
+void adbms_read_cells(ADBMS_bms_t* bms) {
+	float* cell_voltage_ptrs[ADBMS_MODULE_COUNT] = {0};
+	for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+		cell_voltage_ptrs[i] = bms->modules[i].cell_voltages;
+	}
+
+	if (!adbms6380_read_cell_voltages(
+		bms->spi,
+		&bms->tx_strbuf,
+		bms->rx_buf,
+		cell_voltage_ptrs,
+		ADBMS_MODULE_COUNT
+	)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_spi = true;
+		return;
+	}
+
+	bms->max_voltage = cell_voltage_ptrs[0][0];
+	bms->min_voltage = cell_voltage_ptrs[0][0];
+	bms->sum_voltage = 0.0f;
+	for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+		bms->modules[i].max_voltage = cell_voltage_ptrs[i][0];
+		bms->modules[i].min_voltage = cell_voltage_ptrs[i][0];
+		bms->modules[i].sum_voltage = 0.0f;
+		for (size_t j = 0; j < ADBMS6380_CELL_COUNT; j++) {
+			float cell_v = cell_voltage_ptrs[i][j];
+			if (cell_v > bms->modules[i].max_voltage) {
+				bms->modules[i].max_voltage = cell_v;
+			}
+			if (cell_v < bms->modules[i].min_voltage) {
+				bms->modules[i].min_voltage = cell_v;
+			}
+			bms->modules[i].sum_voltage += cell_v;
+		}
+		bms->modules[i].avg_voltage = bms->modules[i].sum_voltage / ADBMS6380_CELL_COUNT;
+
+		if (bms->modules[i].max_voltage > bms->max_voltage) {
+			bms->max_voltage = bms->modules[i].max_voltage;
+		}
+		if (bms->modules[i].min_voltage < bms->min_voltage) {
+			bms->min_voltage = bms->modules[i].min_voltage;
+		}
+		bms->sum_voltage += bms->modules[i].sum_voltage;
+	}
+	bms->avg_voltage = bms->sum_voltage / (ADBMS_MODULE_COUNT * ADBMS6380_CELL_COUNT);
+}
+
+void adbms_read_therms(ADBMS_bms_t* bms) {
+	float* gpio_voltage_ptrs[ADBMS_MODULE_COUNT] = {0};
+	for (size_t i = 0; i < ADBMS_MODULE_COUNT; i++) {
+		gpio_voltage_ptrs[i] = bms->modules[i].thermistors;
+	}
+
+	if (!adbms6380_read_gpio_voltages(
+		bms->spi,
+		&bms->tx_strbuf,
+		bms->rx_buf,
+		gpio_voltage_ptrs,
+		ADBMS_MODULE_COUNT
+	)) {
+		bms->state = ADBMS_STATE_IDLE;
+		bms->err_spi = true;
+		return;
+	}
+}
+
+void adbms_periodic(ADBMS_bms_t* bms, float min_voltage_for_balance, float min_delta_for_balance) {
+	switch (bms->state) {
+		case ADBMS_STATE_IDLE: {
+			adbms_connect(bms);
+			break;
+		}
+		case ADBMS_STATE_CONNECTED: {
+			adbms_read_cells(bms);
+			adbms_read_therms(bms);
+			adbms_balance_and_update_regb(bms, min_voltage_for_balance, min_delta_for_balance);
+			break;
+		}
+	}
+}

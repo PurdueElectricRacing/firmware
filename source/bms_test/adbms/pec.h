c6ed78ffe12179dc57a1ab0db0683355499d9310
LelsersLasers <millankumar@gmail.com>
2026-02-05 18:28:51 -0500

    adding new bms code

---
+++ source/bms_test/adbms/pec.h
@@ -0,0 +1,17 @@
+/**
+ * @file pec.h
+ * @brief Calculation of PEC (CRC15, CRC10) for ADBMS communication.
+ *
+ * @author Analog Devices, Millan Kumar (kumar798@purdue.edu), Irving Wang (irvingw@purdue.edu)
+ */
+
+#ifndef _ADBMS_PEC_H_
+#define _ADBMS_PEC_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+uint16_t adbms_pec_get_pec15(uint8_t len, const uint8_t *data);
+uint16_t adbms_pec_get_pec10(bool bIsRxCmd, uint8_t nLength, const uint8_t *pDataBuf);
+
+#endif // _ADBMS_PEC_H_

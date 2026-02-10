f91b05ff3760db94f7819f1e43ee681c5f9b2b24
LelsersLasers <millankumar@gmail.com>
2026-02-10 16:16:42 -0500

    trying to copy bms abox to run this code on abox g4

--- source/adbms/main.c
+++ source/adbms/main.c
@@ -1,45 +1,59 @@
-/* System Includes */
-#include <math.h>
+#include "main.h"
+
 #include <stdint.h>
 
-#include "common/phal/gpio.h"
-#include "common/phal/spi.h"
+#include "adbms.h"
+#include "common/can_library/faults_common.h"
+#include "common/can_library/generated/A_BOX.h"
 #include "common/freertos/freertos.h"
+#include "common/phal/can.h"
+#include "common/phal/gpio.h"
+#include "common/phal/rcc.h"
 
-/* Module Includes */
-#include "main.h"
-#include "adbms6830/adbms.h"
-#include <string.h>
-#include "adbms6830/commands.h"
-
-dma_init_t spi_rx_dma_config = SPI2_RXDMA_CONT_CONFIG(NULL, 2);
-dma_init_t spi_tx_dma_config = SPI2_TXDMA_CONT_CONFIG(NULL, 1);
+dma_init_t spi_rx_dma_config    = SPI1_RXDMA_CONT_CONFIG(NULL, 2);
+dma_init_t spi_tx_dma_config    = SPI1_TXDMA_CONT_CONFIG(NULL, 1);
 SPI_InitConfig_t bms_spi_config = {
-    .data_len  = 8,
-    .nss_sw = false,
-    .nss_gpio_port = SPI_CS_PORT,
-    .nss_gpio_pin = SPI_CS_PIN,
-    .rx_dma_cfg = &spi_rx_dma_config,
-    .tx_dma_cfg = &spi_tx_dma_config,
-    .periph = SPI2,
+    .data_len      = 8,
+    .nss_sw        = false, // BMS drive CS pin manually to ensure correct timing
+    .nss_gpio_port = SPI1_CS_PORT,
+    .nss_gpio_pin  = SPI1_CS_PIN,
+    .rx_dma_cfg    = &spi_rx_dma_config,
+    .tx_dma_cfg    = &spi_tx_dma_config,
+    .periph        = SPI1,
+    .cpol          = 0,
+    .cpha          = 0,
+    .data_rate     = 1000000, // 1 MHz SPI clock for ADBMS6380
 };
 
+/* PER HAL Initilization Structures */
 GPIOInitConfig_t gpio_config[] = {
-    GPIO_INIT_OUTPUT(SPI_CS_PORT, SPI_CS_PIN, GPIO_OUTPUT_HIGH_SPEED), // PB12
-    GPIO_INIT_AF(SPI_SCK_PORT, SPI_SCK_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN), // PB13
-    GPIO_INIT_AF(SPI_MISO_PORT, SPI_MISO_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_OPEN_DRAIN, GPIO_INPUT_OPEN_DRAIN), // PB14
-    GPIO_INIT_AF(SPI_MOSI_PORT, SPI_MOSI_PIN, 5, GPIO_OUTPUT_HIGH_SPEED, GPIO_OUTPUT_PUSH_PULL, GPIO_INPUT_PULL_DOWN), // PB15
+    // Status LEDs
+    GPIO_INIT_OUTPUT(HEARTBEAT_LED_PORT, HEARTBEAT_LED_PIN, GPIO_OUTPUT_LOW_SPEED),
+    GPIO_INIT_OUTPUT(ERROR_LED_PORT, ERROR_LED_PIN, GPIO_OUTPUT_LOW_SPEED),
+
+    // VCAN
+    GPIO_INIT_FDCAN1RX_PA11,
+    GPIO_INIT_FDCAN1TX_PA12,
+
+    // CCAN
+    GPIO_INIT_FDCAN2RX_PB12,
+    GPIO_INIT_FDCAN2TX_PB13,
+
+    // SPI for BMS
+    GPIO_INIT_OUTPUT(SPI1_CS_PORT, SPI1_CS_PIN, GPIO_OUTPUT_ULTRA_SPEED),
+    GPIO_INIT_SPI1SCK_PA5,
+    GPIO_INIT_SPI1MISO_PA6,
+    GPIO_INIT_SPI1MOSI_PA7,
 };
 
-
-#define TargetCoreClockrateHz 16000000
-ClockRateConfig_t clock_config = {
-    .clock_source           = CLOCK_SOURCE_HSI,
-    .use_pll                = false,
-    .system_clock_target_hz = TargetCoreClockrateHz,
-    .ahb_clock_target_hz    = (TargetCoreClockrateHz / 1),
-    .apb1_clock_target_hz   = (TargetCoreClockrateHz / (1)),
-    .apb2_clock_target_hz   = (TargetCoreClockrateHz / (1)),
+static constexpr uint32_t TargetCoreClockrateHz = 16000000;
+ClockRateConfig_t clock_config                  = {
+                     .clock_source           = CLOCK_SOURCE_HSE,
+                     .use_pll                = false,
+                     .system_clock_target_hz = TargetCoreClockrateHz,
+                     .ahb_clock_target_hz    = (TargetCoreClockrateHz / 1),
+                     .apb1_clock_target_hz   = (TargetCoreClockrateHz / (1)),
+                     .apb2_clock_target_hz   = (TargetCoreClockrateHz / (1)),
 };
 
 /* Locals for Clock Rates */
@@ -63,30 +77,68 @@ void adbms_g_periodic() {
 defineThreadStack(adbms_g_periodic, 200, osPriorityNormal, 2048);
 
 int main(void) {
-    osKernelInitialize();
-
-    /* HAL Initilization */
+    // Hardware Initilization
     if (0 != PHAL_configureClockRates(&clock_config)) {
         HardFault_Handler();
     }
-    if (!PHAL_initGPIO(gpio_config, sizeof(gpio_config) / sizeof(GPIOInitConfig_t))) {
+    if (false == PHAL_initGPIO(gpio_config, sizeof(gpio_config) / sizeof(GPIOInitConfig_t))) {
         HardFault_Handler();
     }
+
+    if (false == PHAL_FDCAN_init(FDCAN1, false, VCAN_BAUD_RATE)) {
+        HardFault_Handler();
+    }
+    if (false == PHAL_FDCAN_init(FDCAN2, false, CCAN_BAUD_RATE)) {
+        HardFault_Handler();
+    }
+
     if (!PHAL_SPI_init(&bms_spi_config)) {
         HardFault_Handler();
     }
+
     g_adbms.spi = &bms_spi_config;
     g_adbms.enable_balance = true;
 
-    // // todo enable user button interrupt
-    // RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+    NVIC_EnableIRQ(FDCAN1_IT0_IRQn);
+    NVIC_EnableIRQ(FDCAN2_IT0_IRQn);
+
+    // Software Initalization
+    osKernelInitialize();
 
     createThread(adbms_g_periodic);
 
+    // no way home
     osKernelStart();
 
     return 0;
 }
+
+
+// int main(void) {
+//     osKernelInitialize();
+
+//     /* HAL Initilization */
+//     if (0 != PHAL_configureClockRates(&clock_config)) {
+//         HardFault_Handler();
+//     }
+//     if (!PHAL_initGPIO(gpio_config, sizeof(gpio_config) / sizeof(GPIOInitConfig_t))) {
+//         HardFault_Handler();
+//     }
+//     if (!PHAL_SPI_init(&bms_spi_config)) {
+//         HardFault_Handler();
+//     }
+//     g_adbms.spi = &bms_spi_config;
+//     g_adbms.enable_balance = true;
+
+//     // // todo enable user button interrupt
+//     // RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+//     createThread(adbms_g_periodic);
+
+//     osKernelStart();
+
+//     return 0;
+// }
 
 void HardFault_Handler() {
     while (1) {
--- source/adbms/main.h
+++ source/adbms/main.h
@@ -1,14 +1,37 @@
+/**
+ * @file main.h
+ * @brief "Abox" node source code
+ * 
+ * @author Irving Wang (irvingw@purdue.edu), Millan Kumar (kumar798@purdue.edu)
+ */
+
 #ifndef MAIN_H_
 #define MAIN_H_
 
-// SPI2
-#define SPI_CS_PORT   (GPIOB)
-#define SPI_SCK_PORT  (GPIOB)
-#define SPI_MISO_PORT (GPIOB)
-#define SPI_MOSI_PORT (GPIOB)
-#define SPI_CS_PIN    (12)
-#define SPI_SCK_PIN   (13)
-#define SPI_MISO_PIN  (14)
-#define SPI_MOSI_PIN  (15)
+#include <stdint.h>
+
+#include "common/phal/gpio.h"
+
+// Status LEDs
+#define HEARTBEAT_LED_PORT  (GPIOB)
+#define HEARTBEAT_LED_PIN   (5)
+#define ERROR_LED_PORT      (GPIOB)
+#define ERROR_LED_PIN       (9)
+#define CONNECTION_LED_PORT (GPIOB)
+#define CONNECTION_LED_PIN  (4)
+
+// SPI1 for BMS
+#define SPI1_SCK_PORT  (GPIOA)
+#define SPI1_MISO_PORT (GPIOA)
+#define SPI1_MOSI_PORT (GPIOA)
+#define SPI1_SCK_PIN   (5)
+#define SPI1_MISO_PIN  (6)
+#define SPI1_MOSI_PIN  (7)
+
+#define SPI1_CS_PORT (GPIOB)
+#define SPI1_CS_PIN  (0)
+
+#define SPI2_CS_PORT (GPIOB)
+#define SPI2_CS_PIN  (1)
 
 #endif

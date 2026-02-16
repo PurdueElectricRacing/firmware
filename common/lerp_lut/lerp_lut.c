628951c704d8531a2acb86951a084e2e68f87b8c
Irving Wang <irvingw@purdue.edu>
2026-02-16 10:15:02 -0500

    smarter binary search

--- common/lerp_lut/lerp_lut.c
+++ common/lerp_lut/lerp_lut.c
@@ -9,23 +9,21 @@ float lut_lookup(const lerp_lut_t* lut, float key) {
     if (key <= keys[0]) { return values[0]; }
     if (key >= keys[size - 1]) { return values[size - 1]; }
 
-    // binary search for the correct interval,
-    // find i such that keys[i] <= key < keys[i + 1]
-    int low = 0;
-    int high = size - 2;
-    int i = 0;
+    // binary search for the correct interval
+    int low = 1;
+    int high = size - 1;
     while (low < high) {
         int mid = low + (high - low) / 2;
 
         if (key < keys[mid]) {
-            high = mid - 1;
-        } else if (key >= keys[mid + 1]) {
-            low = mid + 1;
+            high = mid;
         } else {
-            i = mid;
-            break;
+            low = mid + 1;
         }
     }
+
+    // keys[i] <= key < keys[i + 1]
+    int i = low - 1;
 
     // lerp between values[i] and values[i + 1]
     float t = (key - keys[i]) / (keys[i + 1] - keys[i]);

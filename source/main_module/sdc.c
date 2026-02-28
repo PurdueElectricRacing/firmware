ed8fb1ca671d279b075ca3c3317d180aabb20b88
Irving Wang <irvingw@purdue.edu>
2026-02-27 20:11:39 -0500

    fix mux assignment bug

--- source/main_module/sdc.c
+++ source/main_module/sdc.c
@@ -5,46 +5,36 @@
 #include "common/can_library/faults_common.h"
 #include "common/freertos/freertos.h"
 
-static const fault_index_t SDC_FAULT_LUT[NUM_SDC_NODES - 1] = {
-    FAULT_INDEX_MAIN_MODULE_SDC1_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC2_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC3_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC4_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC5_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC6_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC7_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC8_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC9_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC10_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC11_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC12_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC13_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC14_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC15_OPEN,
-    FAULT_INDEX_MAIN_MODULE_SDC16_OPEN
-    // SDC17 is handled seperately
+typedef struct {
+    fault_index_t fault_index;
+    uint8_t mux_addr;
+} sdc_node_t;
+
+// mux address to indicate the node is inacessible
+static constexpr uint8_t SDC_UNREADABLE = 0xFF; 
+
+// index by the SDC node number (1-17)
+static const sdc_node_t SDC_NODE_LUT[NUM_SDC_NODES] = {
+    {FAULT_INDEX_MAIN_MODULE_SDC1_OPEN,  11},
+    {FAULT_INDEX_MAIN_MODULE_SDC2_OPEN,  10},
+    {FAULT_INDEX_MAIN_MODULE_SDC3_OPEN,  9},
+    {FAULT_INDEX_MAIN_MODULE_SDC4_OPEN,  8},
+    {FAULT_INDEX_MAIN_MODULE_SDC5_OPEN,  7},
+    {FAULT_INDEX_MAIN_MODULE_SDC6_OPEN,  6},
+    {FAULT_INDEX_MAIN_MODULE_SDC7_OPEN,  5},
+    {NULL,  SDC_UNREADABLE}, // SDC8 status cant be checked
+    {FAULT_INDEX_MAIN_MODULE_SDC9_OPEN,  4},
+    {FAULT_INDEX_MAIN_MODULE_SDC10_OPEN, 12},
+    {FAULT_INDEX_MAIN_MODULE_SDC11_OPEN, 3},
+    {FAULT_INDEX_MAIN_MODULE_SDC12_OPEN, 2},
+    {FAULT_INDEX_MAIN_MODULE_SDC13_OPEN, 13},
+    {NULL, SDC_UNREADABLE}, // SDC14 status cant be checked
+    {FAULT_INDEX_MAIN_MODULE_SDC15_OPEN, 1}, // ! schematic inconsitency, SDC15 is marked as SDC14
+    {FAULT_INDEX_MAIN_MODULE_SDC16_OPEN, 14},
+    {FAULT_INDEX_MAIN_MODULE_SDC17_OPEN, 0} // precharge, checked separately
 };
 
-static constexpr uint8_t SDC_UNREADABLE = 0xFF;
-static const uint8_t NODE_TO_MUX_LUT[NUM_SDC_NODES] = {
-    11,             // SDC1
-    10,             // SDC2
-    9,              // SDC3
-    5,              // SDC4
-    7,              // SDC5
-    6,              // SDC6
-    8,              // SDC7
-    4,              // SDC8
-    12,             // SDC9
-    3,              // SDC10
-    2,              // SDC11
-    13,             // SDC12
-    SDC_UNREADABLE, // SDC13
-    1,              // SDC14
-    SDC_UNREADABLE, // SDC15
-    14,             // SDC16
-    0               // SDC17 (precharge)
-};
+// todo: is SDC17 the same as precharge?
 
 void update_SDC() {
     // ! reading the input pin as 1 = closed, 0 = open
@@ -56,8 +46,9 @@ void update_SDC() {
 
     // check SDC state by cycling through the mux and checking the input
     static uint8_t sdc_poll_index = 0;
-    uint8_t mux_addr = NODE_TO_MUX_LUT[sdc_poll_index];
-    bool is_node_open = false; // default to closed if the mux address is invalid
+    const sdc_node_t *current_node = &SDC_NODE_LUT[sdc_poll_index];
+    uint8_t mux_addr = current_node->mux_addr;
+    bool is_node_open = false; // default to closed if the mux address is invalidsdc_node_t
     
     if (mux_addr != SDC_UNREADABLE) {
         // Set mux control
@@ -74,7 +65,7 @@ void update_SDC() {
     
     // Read the signal and update the relevant fault state
     g_SDC_open_nodes[sdc_poll_index] = is_node_open;
-    update_fault(SDC_FAULT_LUT[sdc_poll_index], is_node_open);
+    update_fault(current_node->fault_index, is_node_open);
 
     // update the poll index for the next cycle (mux 0-15 = sdc 1-16)
     sdc_poll_index = (sdc_poll_index + 1) & 0xF;

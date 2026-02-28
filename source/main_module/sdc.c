f60c6f0b1bf0b8b5b763c1a3e244ec6da32e5cdb
Irving Wang <irvingw@purdue.edu>
2026-02-27 19:25:05 -0500

    deal with non sequential mux assignments

--- source/main_module/sdc.c
+++ source/main_module/sdc.c
@@ -25,6 +25,27 @@ static const fault_index_t SDC_FAULT_LUT[NUM_SDC_NODES - 1] = {
     // SDC17 is handled seperately
 };
 
+static constexpr uint8_t SDC_UNREADABLE = 0xFF;
+static const uint8_t NODE_TO_MUX_LUT[NUM_SDC_NODES] = {
+    11,             // SDC1
+    10,             // SDC2
+    9,              // SDC3
+    5,              // SDC4
+    7,              // SDC5
+    6,              // SDC6
+    8,              // SDC7
+    4,              // SDC8
+    12,             // SDC9
+    3,              // SDC10
+    2,              // SDC11
+    13,             // SDC12
+    SDC_UNREADABLE, // SDC13
+    1,              // SDC14
+    SDC_UNREADABLE, // SDC15
+    14,             // SDC16
+    0               // SDC17 (precharge)
+};
+
 void update_SDC() {
     // ! reading the input pin as 1 = closed, 0 = open
 
@@ -35,16 +56,23 @@ void update_SDC() {
 
     // check SDC state by cycling through the mux and checking the input
     static uint8_t sdc_poll_index = 0;
+    uint8_t mux_addr = NODE_TO_MUX_LUT[sdc_poll_index];
+    bool is_node_open = false; // default to closed if the mux address is invalid
+    
+    if (mux_addr != SDC_UNREADABLE) {
+        // Set mux control
+        PHAL_writeGPIO(SDC_MUX_S0_PORT, SDC_MUX_S0_PIN, (mux_addr >> 0) & 0x1);
+        PHAL_writeGPIO(SDC_MUX_S1_PORT, SDC_MUX_S1_PIN, (mux_addr >> 1) & 0x1);
+        PHAL_writeGPIO(SDC_MUX_S2_PORT, SDC_MUX_S2_PIN, (mux_addr >> 2) & 0x1);
+        PHAL_writeGPIO(SDC_MUX_S3_PORT, SDC_MUX_S3_PIN, (mux_addr >> 3) & 0x1);
+        
+        // delay to allow mux signals to stabilize
+        osDelay(1);
 
-    // Set mux control and delay to allow mux signals to stabilize
-    PHAL_writeGPIO(SDC_MUX_S0_PORT, SDC_MUX_S0_PIN, (sdc_poll_index >> 0) & 0x1);
-    PHAL_writeGPIO(SDC_MUX_S1_PORT, SDC_MUX_S1_PIN, (sdc_poll_index >> 1) & 0x1);
-    PHAL_writeGPIO(SDC_MUX_S2_PORT, SDC_MUX_S2_PIN, (sdc_poll_index >> 2) & 0x1);
-    PHAL_writeGPIO(SDC_MUX_S3_PORT, SDC_MUX_S3_PIN, (sdc_poll_index >> 3) & 0x1);
-    osDelay(1);
+        is_node_open = !PHAL_readGPIO(SDC_MUX_PORT, SDC_MUX_PIN);
+    }
     
     // Read the signal and update the relevant fault state
-    bool is_node_open = !PHAL_readGPIO(SDC_MUX_PORT, SDC_MUX_PIN);
     g_SDC_open_nodes[sdc_poll_index] = is_node_open;
     update_fault(SDC_FAULT_LUT[sdc_poll_index], is_node_open);
 

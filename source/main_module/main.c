60f3a1f9a0f9faa6169589958777f1cbd73ff798
LelsersLasers <millankumar@gmail.com>
2026-03-03 20:38:39 -0500

    irq prios

--- source/main_module/main.c
+++ source/main_module/main.c
@@ -149,6 +149,8 @@ int main(void) {
     if (false == PHAL_FDCAN_init(FDCAN3, false, MCAN_BAUD_RATE)) {
         HardFault_Handler();
     }
+    NVIC_SetPriority(FDCAN2_IT0_IRQn, 5);
+    NVIC_SetPriority(FDCAN3_IT0_IRQn, 5);
 
     NVIC_EnableIRQ(FDCAN2_IT0_IRQn);
     NVIC_EnableIRQ(FDCAN3_IT0_IRQn);

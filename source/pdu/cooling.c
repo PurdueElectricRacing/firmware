f083eaed644579f058245946d9ec43c6a562b6e3
Irving Wang <irvingw@purdue.edu>
2026-04-15 15:16:29 -0400

    skeleton fsm

--- source/pdu/cooling.c
+++ source/pdu/cooling.c
@@ -6,26 +6,22 @@
 #include "state.h"
 #include "switches.h"
 
-static void cooling_compute_policy(void) {
-    g_pdu_state.cooling_command.fan1_enabled = true;
-    if (g_pdu_state.cooling_command.batt_fan_autospeed_enabled) {
-        g_pdu_state.cooling_command.fan1_percent = 100;
-    }
+// ! move cooling bang bang into here aswell
 
-    g_pdu_state.cooling_command.fan2_enabled = true;
-    if (g_pdu_state.cooling_command.motor_fan_autospeed_enabled) {
-        g_pdu_state.cooling_command.fan2_percent = 100;
-    }
+typedef enum {
+    COOLING_STATE_AUTO   = 0,
+    COOLING_STATE_MANUAL = 1 // ! leave manual empty for now, auto should be tuned well
+} cooling_state_t;
 
-    g_pdu_state.cooling_command.fan3_enabled = g_pdu_state.cooling_command.fan1_enabled;
-    g_pdu_state.cooling_command.fan3_percent = g_pdu_state.cooling_command.fan1_percent;
-    g_pdu_state.cooling_command.fan4_enabled = g_pdu_state.cooling_command.fan2_enabled;
-    g_pdu_state.cooling_command.fan4_percent = g_pdu_state.cooling_command.fan2_percent;
+static cooling_state_t cooling_state = COOLING_STATE_AUTO;
+static cooling_state_t next_cooling_state = COOLING_STATE_AUTO;
 
-    cooling_bangbang_update(&g_pdu_state.cooling_command);
+static inline void auto_periodic() {
+    // update bang bangs
+
 }
 
-static void cooling_apply_outputs(void) {
+static inline void set_switches(void) {
     switches_set_state(SW_FAN_1, g_pdu_state.cooling_command.fan1_enabled);
     switches_set_state(SW_FAN_2, g_pdu_state.cooling_command.fan2_enabled);
     switches_set_state(SW_FAN_3, g_pdu_state.cooling_command.fan3_enabled);
@@ -33,14 +29,39 @@ static void cooling_apply_outputs(void) {
     switches_set_state(SW_PUMP_1, g_pdu_state.cooling_command.pump1_enabled);
     switches_set_state(SW_PUMP_2, g_pdu_state.cooling_command.pump2_enabled);
     switches_set_state(SW_HXFAN, g_pdu_state.cooling_command.hxfan_enabled);
+}
 
+static inline void set_fans() {
     setFan1Speed(g_pdu_state.cooling_command.fan1_enabled ? g_pdu_state.cooling_command.fan1_percent : 0);
     setFan2Speed(g_pdu_state.cooling_command.fan2_enabled ? g_pdu_state.cooling_command.fan2_percent : 0);
     setFan3Speed(g_pdu_state.cooling_command.fan3_enabled ? g_pdu_state.cooling_command.fan3_percent : 0);
     setFan4Speed(g_pdu_state.cooling_command.fan4_enabled ? g_pdu_state.cooling_command.fan4_percent : 0);
 }
 
-static void cooling_send_outputs(void) {
+void cooling_fsm_periodic() {
+
+    switch(cooling_state) {
+        case COOLING_STATE_AUTO:
+            auto_periodic();
+
+
+            // if dashboard manual override !stale
+                // next_cooling_state = MANUAL
+            break;
+        case COOLING_STATE_MANUAL:
+            // todo update manual controls
+            
+            // if dashboard manual override stale
+                // next_cooling_state = AUTO
+            // else if temps are getting too high
+                // next_cooling_state = AUTO
+            break;
+    }
+
+    set_switches();
+    set_fans();
+
+    // report telemetry
     CAN_SEND_coolant_out(
         g_pdu_state.cooling_command.fan1_percent,
         g_pdu_state.cooling_command.fan2_percent,
@@ -48,14 +69,4 @@ static void cooling_send_outputs(void) {
         g_pdu_state.cooling_command.hxfan_enabled,
         g_pdu_state.cooling_command.pump1_enabled
     );
-}
-
-void cooling_init(void) {
-    cooling_bangbang_init();
-}
-
-void cooling_periodic(void) {
-    cooling_compute_policy();
-    cooling_apply_outputs();
-    cooling_send_outputs();
 }

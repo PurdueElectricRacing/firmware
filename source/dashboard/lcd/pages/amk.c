2e8f9a917913487c24c7636d6b3cc157de6be0cf
amruthn1 <amruthnadimpally1@gmail.com>
2026-04-21 23:35:44 -0400

    make suggestions

--- source/dashboard/lcd/pages/amk.c
+++ source/dashboard/lcd/pages/amk.c
@@ -12,97 +12,80 @@
 #include "nextion.h"
 #include "colors.h"
 
-void reset_inverter_text_colors(char *error_id, char *torque_id, char *on_id) {
-    NXT_setFontColor(error_id, WHITE);
-    NXT_setFontColor(torque_id, WHITE);
-    NXT_setFontColor(on_id, WHITE);
-}
-
-void update_inverter_telemetry(
+static void update_inverter_telemetry(
     char *status_id,
     char *error_id,
-    char *torque_id,
+    char *diagnostic_id,
     char *on_id,
     bool is_stale,
-    bool is_enabled,
-    bool command_inverter_on,
-    bool command_error_reset,
-    int16_t torque_setpoint,
-    bool inverter_on,
-    bool has_error
+    uint8_t inverter_state,
+    bool has_error,
+    uint32_t diagnostic_number,
+    bool inverter_on
 ) {
+    NXT_setFontColor(error_id, WHITE);
+    NXT_setFontColor(diagnostic_id, WHITE);
+    NXT_setFontColor(on_id, WHITE);
+
     if (is_stale) {
         NXT_setFontColor(status_id, RED);
         NXT_setText(status_id, "STALE");
         NXT_setText(error_id, "--");
-        NXT_setText(torque_id, "--");
+        NXT_setText(diagnostic_id, "--");
         NXT_setText(on_id, "--");
         return;
     }
-    
-    NXT_setFontColor(status_id, is_enabled ? GREEN : RED);
-    NXT_setText(status_id, is_enabled ? "ENABLE" : "DISABLE");
-    NXT_setText(on_id, (inverter_on || command_inverter_on) ? "ON" : "OFF");
-    NXT_setText(error_id, has_error ? "ERROR" : (command_error_reset ? "RESET" : "OK"));
-    NXT_setTextFormatted(torque_id, "%d", (int) torque_setpoint);
+
+    NXT_setFontColor(status_id, has_error ? RED : GREEN);
+    NXT_setTextFormatted(status_id, "%u", (unsigned int) inverter_state);
+    NXT_setText(error_id, has_error ? "ERROR" : "OK");
+    NXT_setTextFormatted(diagnostic_id, "%lu", (unsigned int) diagnostic_number);
+    NXT_setText(on_id, inverter_on ? "ON" : "OFF");
 }
 
 void amk_telemetry_update() {
-    reset_inverter_text_colors(INVA_ERROR, INVA_DIAGNOSTIC, INVA_ON);
-    reset_inverter_text_colors(INVB_ERROR, INVB_DIAGNOSTIC, INVB_ON);
-    reset_inverter_text_colors(INVC_ERROR, INVC_DIAGNOSTIC, INVC_ON);
-    reset_inverter_text_colors(INVD_ERROR, INVD_DIAGNOSTIC, INVD_ON);
-
     update_inverter_telemetry(
         INVA_STATUS,
         INVA_ERROR,
         INVA_DIAGNOSTIC,
         INVA_ON,
-        can_data.inva_state_diag_vcan.is_stale(),
-        can_data.inva_state_diag_vcan.amk_control_enable,
-        can_data.inva_state_diag_vcan.amk_control_inverter_on,
-        can_data.inva_state_diag_vcan.amk_control_error_reset,
-        can_data.inva_state_diag_vcan.amk_torque_setpoint,
-        can_data.inva_state_diag_vcan.amk_state_inverter_on,
-        can_data.inva_state_diag_vcan.amk_state_error
+        can_data.inva_diagnostics.is_stale(),
+        can_data.inva_diagnostics.inverter_state,
+        can_data.inva_diagnostics.bError,
+        can_data.inva_diagnostics.diagnostic_number,
+        can_data.inva_diagnostics.InverterOn
     );
     update_inverter_telemetry(
         INVB_STATUS,
         INVB_ERROR,
         INVB_DIAGNOSTIC,
         INVB_ON,
-        can_data.invb_state_diag_vcan.is_stale(),
-        can_data.invb_state_diag_vcan.amk_control_enable,
-        can_data.invb_state_diag_vcan.amk_control_inverter_on,
-        can_data.invb_state_diag_vcan.amk_control_error_reset,
-        can_data.invb_state_diag_vcan.amk_torque_setpoint,
-        can_data.invb_state_diag_vcan.amk_state_inverter_on,
-        can_data.invb_state_diag_vcan.amk_state_error
+        can_data.invb_diagnostics.is_stale(),
+        can_data.invb_diagnostics.inverter_state,
+        can_data.invb_diagnostics.bError,
+        can_data.invb_diagnostics.diagnostic_number,
+        can_data.invb_diagnostics.InverterOn
     );
     update_inverter_telemetry(
         INVC_STATUS,
         INVC_ERROR,
         INVC_DIAGNOSTIC,
         INVC_ON,
-        can_data.invc_state_diag_vcan.is_stale(),
-        can_data.invc_state_diag_vcan.amk_control_enable,
-        can_data.invc_state_diag_vcan.amk_control_inverter_on,
-        can_data.invc_state_diag_vcan.amk_control_error_reset,
-        can_data.invc_state_diag_vcan.amk_torque_setpoint,
-        can_data.invc_state_diag_vcan.amk_state_inverter_on,
-        can_data.invc_state_diag_vcan.amk_state_error
+        can_data.invc_diagnostics.is_stale(),
+        can_data.invc_diagnostics.inverter_state,
+        can_data.invc_diagnostics.bError,
+        can_data.invc_diagnostics.diagnostic_number,
+        can_data.invc_diagnostics.InverterOn
     );
     update_inverter_telemetry(
         INVD_STATUS,
         INVD_ERROR,
         INVD_DIAGNOSTIC,
         INVD_ON,
-        can_data.invd_state_diag_vcan.is_stale(),
-        can_data.invd_state_diag_vcan.amk_control_enable,
-        can_data.invd_state_diag_vcan.amk_control_inverter_on,
-        can_data.invd_state_diag_vcan.amk_control_error_reset,
-        can_data.invd_state_diag_vcan.amk_torque_setpoint,
-        can_data.invd_state_diag_vcan.amk_state_inverter_on,
-        can_data.invd_state_diag_vcan.amk_state_error
+        can_data.invd_diagnostics.is_stale(),
+        can_data.invd_diagnostics.inverter_state,
+        can_data.invd_diagnostics.bError,
+        can_data.invd_diagnostics.diagnostic_number,
+        can_data.invd_diagnostics.InverterOn
     );
 }

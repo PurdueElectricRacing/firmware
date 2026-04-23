1b5e75860094e563c7b10e71e546786940d05a99
Irving Wang <irvingw@purdue.edu>
2026-04-23 14:14:21 -0400

    draft

---
+++ common/ublox/nav_relposned.c
@@ -0,0 +1,18 @@
+#include "nav_relposned.h"
+#include <string.h>
+
+void NAV_RELPOSNED_decode(NAV_RELPOSNED_data_t *nav_relposned, const uint8_t *rx_buffer) {
+    bool is_header_valid =
+        (rx_buffer[0] == NAV_RELPOSNED_HEADER_B0) &&
+        (rx_buffer[1] == NAV_RELPOSNED_HEADER_B1) &&
+        (rx_buffer[2] == NAV_RELPOSNED_CLASS) &&
+        (rx_buffer[3] == NAV_RELPOSNED_MSG_ID);
+
+    if (!is_header_valid) {
+        return;
+    }
+
+    memcpy(nav_relposned, (rx_buffer + 6), sizeof(NAV_RELPOSNED_data_t));
+
+    // todo validate checksum
+}

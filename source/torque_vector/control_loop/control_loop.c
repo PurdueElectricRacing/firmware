f1d0e74fcc9d55307c28cbb2ebd8e9d47d92ebda
Irving Wang <irvingw@purdue.edu>
2026-05-02 08:53:45 -0400

    some corrections

--- source/torque_vector/control_loop/control_loop.c
+++ source/torque_vector/control_loop/control_loop.c
@@ -19,34 +19,28 @@ static yVCU_struct yVCU;
 
 // init default settings
 volatile vcu_mode_t vcu_mode = VCU_MODE_AUTOCROSS;
+volatile bool is_tv_enabled = false;
+volatile bool is_regen_enabled = false;
 volatile vcu_settings_data_t vcu_settings[4] = {
     [VCU_MODE_ACCEL] = {
         .lateral_gain = 50,
         .longitudinal_gain = 50,
-        .electronic_brake_bias = 50,
-        .is_regen_enabled = true,
-        .is_tv_enabled = true
+        .electronic_brake_bias = 50
     },
     [VCU_MODE_SKIDPAD] = {
         .lateral_gain = 50,
         .longitudinal_gain = 50,
-        .electronic_brake_bias = 50,
-        .is_regen_enabled = true,
-        .is_tv_enabled = true
+        .electronic_brake_bias = 50
     },
     [VCU_MODE_AUTOCROSS] = {
         .lateral_gain = 50,
         .longitudinal_gain = 50,
-        .electronic_brake_bias = 50,
-        .is_regen_enabled = true,
-        .is_tv_enabled = true
+        .electronic_brake_bias = 50
     },
     [VCU_MODE_ENDURANCE] = {
         .lateral_gain = 50,
         .longitudinal_gain = 50,
-        .electronic_brake_bias = 50,
-        .is_regen_enabled = true,
-        .is_tv_enabled = true
+        .electronic_brake_bias = 50
     }
 };
 uint32_t last_vcu_settings_tx = 0;
@@ -68,8 +62,8 @@ static inline void report_vcu_settings() {
         current_settings->lateral_gain,
         current_settings->longitudinal_gain,
         current_settings->electronic_brake_bias,
-        current_settings->is_regen_enabled,
-        current_settings->is_tv_enabled
+        is_regen_enabled,
+        is_tv_enabled
     );
 }
 
@@ -83,8 +77,8 @@ void vcu_driver_request_CALLBACK(void) {
     vcu_settings[vcu_mode].lateral_gain = can_data.vcu_driver_request.lateral_gain;
     vcu_settings[vcu_mode].longitudinal_gain = can_data.vcu_driver_request.longitudinal_gain;
     vcu_settings[vcu_mode].electronic_brake_bias = can_data.vcu_driver_request.electronic_brake_bias;
-    vcu_settings[vcu_mode].is_regen_enabled = can_data.vcu_driver_request.is_regen_enabled;
-    vcu_settings[vcu_mode].is_tv_enabled = can_data.vcu_driver_request.is_tv_enabled;
+    is_regen_enabled = can_data.vcu_driver_request.is_regen_enabled;
+    is_tv_enabled = can_data.vcu_driver_request.is_tv_enabled;
 
     // echo back the current settings
     report_vcu_settings();
@@ -97,11 +91,19 @@ void control_loop() {
         last_vcu_settings_tx = now;
     }
 
+    bool is_crit_stale = can_data.pedals.is_stale() || can_data.steering_angle.is_stale() ||
+                         can_data.wheel_speeds.is_stale() || can_data.IZZE_angular_rate.is_stale();
+    if (is_crit_stale) {
+        is_tv_enabled = false;
+        return;
+    }
+
     // load up the xVCU (input) struct with most recent data
     xVCU.VCU_MODE_REQ = vcu_mode;
 
-    xVCU.THROT_RAW = can_data.pedals.throttle / 4095.0f / 100.0f; // scale to [1,0]
-    xVCU.BRAKE_RAW = can_data.pedals.brake / 4095.0f / 100.0f * -1.0f; // scale to [0,-1]
+    xVCU.THROT_RAW = can_data.pedals.throttle / 100.0f;
+    xVCU.REGEN_RAW = can_data.pedals.regen / 100.0f;
+    xVCU.BRAKE_RAW = can_data.pedals.brake / 100.0f;
 
     xVCU.ST_RAW = can_data.steering_angle.angle * UNPACK_COEFF_STEERING_ANGLE_ANGLE;
     xVCU.VB_RAW = can_data.pack_stats.pack_voltage * UNPACK_COEFF_PACK_STATS_PACK_VOLTAGE;
@@ -110,9 +112,11 @@ void control_loop() {
     xVCU.WM_RAW[2] = can_data.wheel_speeds.rear_left;
     xVCU.WM_RAW[3] = can_data.wheel_speeds.rear_right;
     xVCU.GS_RAW = nav_pvt.groundSpeed * (1000.0f); // convert mm/s to m/s
-    xVCU.AV_RAW[0] = imu_data.gyro_x * UNPACK_COEFF_IMU_ANGULAR_RATE_X_AXIS;
-    xVCU.AV_RAW[1] = imu_data.gyro_y * UNPACK_COEFF_IMU_ANGULAR_RATE_Y_AXIS;
-    xVCU.AV_RAW[2] = imu_data.gyro_z * UNPACK_COEFF_IMU_ANGULAR_RATE_Z_AXIS;
+
+    static constexpr float deg2rad = 180.0f / 3.14f;
+    xVCU.AV_RAW[0] = imu_data.gyro_x * UNPACK_COEFF_IMU_ANGULAR_RATE_X_AXIS * deg2rad;
+    xVCU.AV_RAW[1] = imu_data.gyro_y * UNPACK_COEFF_IMU_ANGULAR_RATE_Y_AXIS * deg2rad;
+    xVCU.AV_RAW[2] = imu_data.gyro_z * UNPACK_COEFF_IMU_ANGULAR_RATE_Z_AXIS * deg2rad;
     xVCU.IB_RAW = can_data.pack_stats.pack_current * UNPACK_COEFF_PACK_STATS_PACK_CURRENT;
 
     int16_t max_motor_temp = MAXOF(
@@ -133,9 +137,14 @@ void control_loop() {
     int16_t scaled_igbt_temp = max_igbt_temp * UNPACK_COEFF_IGBT_TEMPS_FRONT_RIGHT;
     xVCU.IGBT_T_RAW = scaled_igbt_temp;
 
-    xVCU.BT_RAW = 30.0f; // hardcoded to 30C
+    xVCU.BT_RAW = 30.0f; // ! hardcoded to 30C for now
 
-    xVCU.RG_split_FR_RAW = 0.3f; // todo driver configurable
+    volatile vcu_settings_data_t *current_settings = &vcu_settings[vcu_mode];
+    xVCU.RG_FR_split_RAW = current_settings->electronic_brake_bias;
+    xVCU.SK_LR_gain_RAW = vcu_settings[VCU_MODE_SKIDPAD].lateral_gain;
+    xVCU.SK_FR_split_RAW = vcu_settings[VCU_MODE_SKIDPAD].longitudinal_gain;
+    xVCU.AX_LR_gain_RAW = vcu_settings[VCU_MODE_AUTOCROSS].lateral_gain;
+    xVCU.SK_FR_split_RAW = vcu_settings[VCU_MODE_AUTOCROSS].longitudinal_gain;
 
     // step the VCU model
     vcu_step(&pVCU, &xVCU, &yVCU);

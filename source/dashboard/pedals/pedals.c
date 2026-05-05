252a956e38fbafbdc1218a589eedc6979a9c2d3c
Irving Wang <irvingw@purdue.edu>
2026-05-05 15:59:52 -0400

    clarify comments

--- source/dashboard/pedals/pedals.c
+++ source/dashboard/pedals/pedals.c
@@ -36,8 +36,8 @@ static constexpr uint16_t REGEN1_MAX = 3350;
 
 static constexpr uint8_t PEDAL_MAX = 100;
 static constexpr uint8_t PEDAL_MIN = 0;
-static constexpr uint8_t APPS_THROTTLE_THRESHOLD = 30; // 10%
-static constexpr uint8_t APPS_BRAKE_THRESHOLD = PEDAL_MAX / 10u; // 10%
+static constexpr uint8_t APPS_THROTTLE_THRESHOLD = 25; // 25% travel
+static constexpr uint8_t APPS_MECH_BRAKE_THRESHOLD = 80; // 80% travel of the regen pot
 
 // Contains the current pedal values for global visibility
 volatile pedals_data_t pedal_values = {
@@ -52,43 +52,42 @@ volatile pedals_data_t pedal_values = {
  * @note This function is called periodically by the scheduler
  */
 void pedals_periodic(void) {
-    // Get current values (don't want them changing mid-calculation)
+    // snapshot ADC values into local memory
     uint16_t throttle1 = raw_adc_values.throttle1;
     uint16_t throttle2 = 4095 - raw_adc_values.throttle2; // Invert value for t2 (pull-up resistor)
     uint16_t regen1    = raw_adc_values.regen1;
     // uint16_t brake2 = raw_adc_values.brake2_pressure;
 
-    // FSAE 2026 T.4.2.10
+    // FSAE 2026 T.4.2.10: open/short circuit detection
     update_fault(FAULT_ID_APPS_WIRING_T1, 1);
     update_fault(FAULT_ID_APPS_WIRING_T2, 1);
 
-    // Hard clamp the raw values to the min and max values to account for physical limits
+    // saturate the raw values to the calibration range
     throttle1 = CLAMP(throttle1, THROTTLE1_MIN, THROTTLE1_MAX);
     throttle2 = CLAMP(throttle2, THROTTLE2_MIN, THROTTLE2_MAX);
     regen1    = CLAMP(regen1, REGEN1_MIN, REGEN1_MAX);
     // brake2 = CLAMP(brake2, BRAKE2_MIN, BRAKE2_MAX);
 
-    // Rescale pedal signals to the 0-100 range
+    // rescale the pedal signals to [0,100] range
     throttle1 = RESCALE(throttle1, THROTTLE1_MIN, THROTTLE1_MAX, PEDAL_MIN, PEDAL_MAX);
     throttle2 = RESCALE(throttle2, THROTTLE2_MIN, THROTTLE2_MAX, PEDAL_MIN, PEDAL_MAX);
     regen1    = RESCALE(regen1, REGEN1_MIN, REGEN1_MAX, PEDAL_MIN, PEDAL_MAX);
     // brake2 = RESCALE(brake2, BRAKE2_MIN, BRAKE2_MAX, 0, 100);
-    
-    pedal_values.throttle = throttle1;
-    pedal_values.regen = regen1;
 
+    pedal_values.throttle    = throttle1;
+    pedal_values.regen       = regen1;
     uint8_t throttle_command = throttle1;
 
-    // FSAE 2026 T.4.2.5
+    // FSAE 2026 T.4.2.5: if the two throttle sensors differ by 10%, trigger implaus
     int throttle_diff = ABS((int)throttle1 - (int)throttle2);
     update_fault(FAULT_ID_APPS_IMPLAUSIBLE, throttle_diff); // ! disabled for now
     if (is_latched(FAULT_ID_APPS_IMPLAUSIBLE)) {
         throttle_command = 0;
     }
 
-    // If both pedals are pressed, set throttle to 0
-    // todo FSAE 2026 EV.4.7
-    bool is_brake_pressed = pedal_values.regen >= APPS_BRAKE_THRESHOLD; // regen is the first throw of the brake
+    // FSAE 2026 EV.4.7: if both pedals are pressed, set throttle to 0
+    // todo: unlatch when throttle is released to 5%
+    bool is_brake_pressed = pedal_values.regen >= APPS_MECH_BRAKE_THRESHOLD;
     bool is_throttle_pressed = pedal_values.throttle >= APPS_THROTTLE_THRESHOLD;
     update_fault(FAULT_ID_APPS_BRAKE, is_brake_pressed && is_throttle_pressed);
     if (is_latched(FAULT_ID_APPS_BRAKE)) {

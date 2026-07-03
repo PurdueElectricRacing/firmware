3c085dba3d1112ef3b4eec623605664abbf96cfa
irving <irvingw@purdue.edu>
2026-07-03 16:17:21 -0400

    leverage fault system

--- source/torque_vector/sensors/gps.c
+++ source/torque_vector/sensors/gps.c
@@ -5,6 +5,7 @@
  * @author Irving Wang (irvingw@purdue.edu)
  */
 
+#include "can_library/faults_common.h"
 #include "common/freertos/freertos.h"
 #include "sensors.h"
 
@@ -18,6 +19,20 @@ void gps_periodic(void) {
 
     last_gps_message_time_ms = xTaskGetTickCount();
 
+    // decode raw data stream
     NAV_PVT_decode(&nav_pvt, rover_rx_buffer);
     NAV_RELPOSNED_decode(&nav_relposned, (rover_rx_buffer + NAV_PVT_TOTAL_LENGTH));
+
+    // parse fix flags
+    bool is_fix_ok = nav_pvt.flags1 & GPS_FLAG1_GNSS_FIX_OK;
+    update_fault(FAULT_ID_GPS_INVALID_FIX, !is_fix_ok);
+
+    bool is_strong_fix =
+        (nav_pvt.fixType == GPS_FIX_TYPE_GNSS_2D) ||
+        (nav_pvt.fixType == GPS_FIX_TYPE_GNSS_3D) ||
+        (nav_pvt.fixType == GPS_FIX_TYPE_GNSS_DEAD_RECKONING); 
+    update_fault(FAULT_ID_GPS_WEAK_FIX, !is_strong_fix);
+
+    bool is_UTC_resolved = nav_pvt.valid & GPS_VALID_FULLY_RESOLVED;
+    update_fault(FAULT_ID_GPS_INVALID_UTC, !is_UTC_resolved);
 }
--- source/torque_vector/telemetry/telemetry.c
+++ source/torque_vector/telemetry/telemetry.c
@@ -7,9 +7,10 @@
 
 #include "telemetry.h"
 
+#include "can_library/faults_common.h"
 #include "can_library/generated/TORQUE_VECTOR.h"
-#include "sensors.h"
 #include "common/utils/clamp.h"
+#include "sensors.h"
 
 /**
  * @brief Reports telemetry data at 100HZ rate
@@ -19,12 +20,7 @@ static_assert(GPS_COORDINATES_PERIOD_MS == TELEMETRY_100HZ_PERIOD_MS);
 static_assert(GPS_VELOCITY_PERIOD_MS == TELEMETRY_100HZ_PERIOD_MS);
 static_assert(GPS_SPEED_HEADING_PERIOD_MS == TELEMETRY_100HZ_PERIOD_MS);
 void report_telemetry_100hz(void) {
-    bool is_gps_locked =
-        (nav_pvt.fixType == GPS_FIX_TYPE_GNSS_2D) ||
-        (nav_pvt.fixType == GPS_FIX_TYPE_GNSS_3D) ||
-        (nav_pvt.fixType == GPS_FIX_TYPE_GNSS_DEAD_RECKONING);
-
-    if (is_gps_locked) {
+    if (is_clear(FAULT_ID_GPS_INVALID_FIX) && is_clear(FAULT_ID_GPS_WEAK_FIX)) {
         CAN_SEND_gps_coordinates(nav_pvt.latitude, nav_pvt.longitude);
         CAN_SEND_gps_velocity(nav_pvt.velNorth, nav_pvt.velEast);
         CAN_SEND_gps_speed_heading(nav_pvt.groundSpeed, nav_pvt.headingVehicle);
@@ -37,9 +33,7 @@ void report_telemetry_100hz(void) {
  */
 static_assert(GPS_TIME_PERIOD_MS == TELEMETRY_1HZ_PERIOD_MS);
 void report_telemetry_1hz(void) {
-    bool is_gps_time_synced = nav_pvt.valid & GPS_VALID_FULLY_RESOLVED;
-
-    if (is_gps_time_synced) {
+    if (is_clear(FAULT_ID_GPS_INVALID_FIX) && is_clear(FAULT_ID_GPS_INVALID_UTC)) {
         CAN_SEND_gps_time(
             (uint8_t)(nav_pvt.year - 2000),
             nav_pvt.month,

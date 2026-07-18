2d7689f1673fe795c815471c69ebb742ae520c60
LelsersLasers <millankumar@gmail.com>
2026-07-18 11:25:35 -0700

    vApplicationGetIdleTaskMemory & vApplicationGetTimerTaskMemory
    We also don't use the idle hook so disabling it

--- common/freertos/FreeRTOSConfig.h
+++ common/freertos/FreeRTOSConfig.h
@@ -71,7 +71,7 @@ extern uint32_t SystemCoreClock;
 
 #define configUSE_PREEMPTION 1
 #define configUSE_TIME_SLICING 1
-#define configUSE_IDLE_HOOK 1
+#define configUSE_IDLE_HOOK 0
 #define configUSE_TICK_HOOK 0
 #define configSUPPORT_STATIC_ALLOCATION 1
 #define configSUPPORT_DYNAMIC_ALLOCATION 0
--- common/freertos/freertos.c
+++ common/freertos/freertos.c
@@ -26,3 +26,31 @@ void periodic_task_runner(void *arg) {
     // Unreachable!
     vTaskDelete(NULL);
 }
+
+
+void vApplicationGetIdleTaskMemory(
+    StaticTask_t **ppxIdleTaskTCBBuffer,
+    StackType_t **ppxIdleTaskStackBuffer,
+    uint32_t *pulIdleTaskStackSize
+) {
+    static StaticTask_t xIdleTaskTCB;
+    static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+    *ppxIdleTaskStackBuffer = xIdleStack;
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+
+
+void vApplicationGetTimerTaskMemory(
+    StaticTask_t **ppxTimerTaskTCBBuffer,
+    StackType_t **ppxTimerTaskStackBuffer,
+    uint32_t *pulTimerTaskStackSize
+) {
+    static StaticTask_t xTimerTaskTCB;
+    static StackType_t xTimerStack[configTIMER_TASK_STACK_DEPTH];
+
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+    *ppxTimerTaskStackBuffer = xTimerStack;
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}

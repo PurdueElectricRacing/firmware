296af63be429d69d8c8cdea085ed7319f7bf815e
LelsersLasers <millankumar@gmail.com>
2026-07-24 21:36:06 -0700

    GPIO bank enabled by GPIO hal

--- common/phal_G4/fdcan/fdcan.c
+++ common/phal_G4/fdcan/fdcan.c
@@ -14,10 +14,6 @@
 void PHAL_FDCAN_init(FDCAN_GlobalTypeDef *fdcan, PHAL_FDCAN_BaudRate_t bit_rate) {
     PHAL_FDCAN_priv_enableClock();
 
-    // All FDCAN peripherals are on GPIO Bank B
-    // So unconditionally enable it
-    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
- 
     PHAL_FDCAN_priv_enterConfig(fdcan);
  
     // Classic CAN config (no FD/BRS/loopback, auto-retransmission enabled, TX pause on)

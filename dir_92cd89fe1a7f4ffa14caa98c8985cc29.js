9214312e328bc482b039d66eb8d9a80020d4b872
irvingywang <irvingywang@users.noreply.github.com>
2026-07-25 07:39:53 +0000

    deploy: 3738ee3ff5a0e8de7bb8fd47c21982f695e6fb52

--- dir_92cd89fe1a7f4ffa14caa98c8985cc29.js
+++ dir_92cd89fe1a7f4ffa14caa98c8985cc29.js
@@ -1,7 +1,9 @@
 var dir_92cd89fe1a7f4ffa14caa98c8985cc29 =
 [
+    [ "powertrain", "dir_fde07448f8c4f29a4a274a448993566d.html", "dir_fde07448f8c4f29a4a274a448993566d" ],
     [ "sdc", "dir_e44a99400c98fdc940c6db3c014c8b51.html", "dir_e44a99400c98fdc940c6db3c014c8b51" ],
     [ "telemetry", "dir_716eb21476e6667075ada1cffbb2e013.html", "dir_716eb21476e6667075ada1cffbb2e013" ],
+    [ "torque_controller", "dir_226f31282dd85db28d51705ca5742752.html", "dir_226f31282dd85db28d51705ca5742752" ],
     [ "vehicle_fsm", "dir_56133064f5814fde0698e97b1f76cfc6.html", "dir_56133064f5814fde0698e97b1f76cfc6" ],
     [ "main.c", "main__module_2main_8c.html", null ],
     [ "main.h", "main__module_2main_8h.html", null ]

bb7117a37d57a301e40e497b834320767019254a
LelsersLasers <millankumar@gmail.com>
2026-08-02 10:06:44 -0700

    Fix doxygen style

--- common/phal_G4/dma/dma.h
+++ common/phal_G4/dma/dma.h
@@ -117,44 +117,45 @@ typedef struct {
  * @brief Initialize DMA peripheral to set m2m, p2p, or p2m with set size
  *        and length of txfer
  *
- * @param dma -> Address of initialization structure
- * @return true -> Successful init (no clashing params)
- * @return false -> Init not complete (parameters clash)
+ * @param dma Address of initialization structure
+ * @return true if successful init (no clashing params) or false if init failed (clashing params)
  */
 bool PHAL_initDMA(dma_init_t* dma);
 
 /**
  * @brief Start txfer after sucessful DMA peripheral initialization
  *
- * @param dma -> Address of initialization structure
+ * @param dma Address of initialization structure
  */
 void PHAL_DMA_startTxfer(dma_init_t* dma);
 
 /**
  * @brief Stop txfer
  *
- * @param dma -> Address of initialization structure
+ * @param dma Address of initialization structure
  */
 void PHAL_DMA_stopTxfer(dma_init_t* dma);
 
 /**
  * @brief Re-enable DMA txfer after error ISR fires
  *
- * @param dma -> Address of initialization structure
+ * @param dma Address of initialization structure
  */
 void PHAL_DMA_reEnable(dma_init_t* dma);
 
 /**
  * @brief Set memory address for DMA transfer. In Mem to Mem this acts as the source address
  *
- * @param dma -> Address of initialization structure
+ * @param dma Address of initialization structure
+ * @param address Memory address to set for DMA transfer
  */
 void PHAL_DMA_setMemAddress(dma_init_t* dma, const uint32_t address);
 
 /**
  * @brief Set transfer length for DMA transaction
  *
- * @param dma -> Address of initialization structure
+ * @param dma Address of initialization structure
+ * @param length Number of data elements to transfer
  */
 void PHAL_DMA_setTxferLength(dma_init_t* dma, const uint32_t length);
 

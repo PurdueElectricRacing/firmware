2295af858cbdfe120c1feba27b394ededa3e9d90
LelsersLasers <millankumar@gmail.com>
2026-08-02 23:35:16 -0700

    re-add SPI init fail to PDU

--- source/pdu/main.c
+++ source/pdu/main.c
@@ -308,7 +308,9 @@ int main() {
 
     CAN_init();
 
-    PHAL_SPI_init(&spi_config);
+    if (!PHAL_SPI_init(&spi_config)) {
+        HardFault_Handler();
+    }
 
     PHAL_writeGPIO(LED_CTRL_BLANK_GPIO_Port, LED_CTRL_BLANK_Pin, 1);
 
